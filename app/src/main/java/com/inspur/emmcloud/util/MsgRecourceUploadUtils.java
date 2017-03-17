ec2798ed953b441603a1ab26f74cc2d7b0322c77
Chen Mingchao <chenmch@inspur.com>
2017-03-17 10:59:08 +0800

    修改channel图片上传逻辑

--- app/src/main/java/com/inspur/emmcloud/util/MsgRecourceUploadUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/MsgRecourceUploadUtils.java
@@ -13,14 +13,13 @@ import com.inspur.emmcloud.bean.Msg;
 import com.inspur.emmcloud.config.MyAppConfig;
 import com.inspur.imp.plugin.camera.imagepicker.ImagePicker;
 import com.inspur.imp.plugin.camera.imagepicker.bean.ImageItem;
+import com.inspur.imp.util.imgcompress.Compressor;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 
 public class MsgRecourceUploadUtils {
 	
@@ -38,6 +37,9 @@ public class MsgRecourceUploadUtils {
 		String filePath = "";
 		if (data.hasExtra("save_file_path")) {
 			filePath = data.getStringExtra("save_file_path");
+			String fileName = System.currentTimeMillis() + ".jpg";
+			new Compressor.Builder(context).setMaxWidth(1200).setMaxHeight(1200).setQuality(90).setDestinationDirectoryPath(MyAppConfig.LOCAL_CACHE_PATH).setFileName(fileName).build().compressToFile(new File(filePath));
+			filePath = MyAppConfig.LOCAL_CACHE_PATH+fileName;
 		}else {
 			ArrayList<ImageItem> imageItemList = (ArrayList<ImageItem>) data
 					.getSerializableExtra(ImagePicker.EXTRA_RESULT_ITEMS);
@@ -46,17 +48,17 @@ public class MsgRecourceUploadUtils {
 		//暂时为要显示的消息设定一个假的id
 		String fakeMessageId = System.currentTimeMillis() + "";
 		String uploadFilePath = filePath;
-		try {
-			String thumbDirPath = MyAppConfig.LOCAL_CACHE_PATH;
-			FileUtils.makeDirs(thumbDirPath);
-			String thumbFilePath = thumbDirPath + new Date().getTime() + ".jpg";
-			ImageUtils.createImageThumbnail(context,
-					filePath, thumbFilePath, 1200, 80);
-			uploadFilePath = thumbFilePath;
-		} catch (IOException e) {
-			e.printStackTrace();
-
-		}
+//		try {
+//			String thumbDirPath = MyAppConfig.LOCAL_CACHE_PATH;
+//			FileUtils.makeDirs(thumbDirPath);
+//			String thumbFilePath = thumbDirPath + new Date().getTime() + ".jpg";
+//			ImageUtils.createImageThumbnail(context,
+//					filePath, thumbFilePath, 1200, 80);
+//			uploadFilePath = thumbFilePath;
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//
+//		}
 		uploadFile(context, apiService, uploadFilePath, fakeMessageId, true);
 		File uploadFile = new File(uploadFilePath);
 		uploadFilePath = "file://" + uploadFilePath;
