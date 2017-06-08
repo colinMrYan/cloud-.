3a69351a2fa5cd7df12fc99d95453b85ee7b4a1c
Yu Fuchang <yufuchang@inspur.com>
2017-06-08 11:21:49 +0800

    修改显示mentions的显示方案

--- app/src/main/java/com/inspur/emmcloud/util/MentionsAndUrlShowUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/MentionsAndUrlShowUtils.java
@@ -39,20 +39,44 @@ public class MentionsAndUrlShowUtils {
 			int index = -1;
 			index = mentions.indexOf(patternString);
 			boolean hasProtocol = false;
-			//修改正则表达式，和IOS一致，注释掉的为原来表达式
-//			Pattern patternProtocol = Pattern.compile("\\(.*\\)");
-			Pattern patternProtocol = Pattern.compile("\\((((https?|ec[cm](-[0-9a-z]+)+|gs-msg)://[a-zA-Z0-9\\_\\-]+(\\.[a-zA-Z0-9\\_\\-]+)*(\\:\\d{2,4})?(/?[a-zA-Z0-9\\-\\_\\.\\?\\=\\&\\%\\#]+)*/?)|([a-zA-Z0-9\\-\\_]+\\.)+([a-zA-Z\\-\\_]+)(\\:\\d{2,4})?(/?[a-zA-Z0-9\\-\\_\\.\\?\\=\\&\\%\\#]+)*/?|\\d+(\\.\\d+){3}(\\:\\d{2,4})?)\\)\n");
-			Matcher matcherProtocol = patternProtocol.matcher(patternString);
-			while (matcherProtocol.find()) {
-				protocolResource = matcherProtocol.group();
-				if(protocolResource.startsWith("(ecm-contact://")){
-					protocol = protocolResource.replace("(", "").replace(")", "");
-					protocolResource = protocolResource.replace("ecm-contact://", "")
-							.replace("(", "\"").replace(")", "\"");
-				}else {
-					protocol = protocolResource.replace("(", "").replace(")", "");
+
+			StringBuilder stringBuilder = new StringBuilder();
+			char  leftBrackets  = '(';
+			for(int i = patternString.length() - 1; i > 0; i--){
+				char item = patternString.charAt(i);
+				stringBuilder.append(item);
+				if(item == leftBrackets){
+					break;
 				}
 			}
+			if(stringBuilder.toString().length() > 0){
+				protocolResource = stringBuilder.reverse().toString();
+				if(stringBuilder.toString().startsWith("(ecm-contact://")){
+					protocol = stringBuilder.toString().replace("(", "").replace(")", "");
+					protocolResource = stringBuilder.toString().replace("ecm-contact://", "")
+							.replace("(", "\"").replace(")", "\"");
+				}else {
+					protocol = stringBuilder.toString().replace("(", "").replace(")", "");
+				}
+			}
+			//废掉的逻辑
+			//修改正则表达式，和IOS一致，注释掉的为原来表达式
+//			Pattern patternProtocol = Pattern.compile("\\(.*\\)");
+//			Pattern patternProtocol = Pattern.compile("\\((((https?|ec[cm](-[0-9a-z]+)+|gs-msg)://[a-zA-Z0-9\\_\\-]+(\\.[a-zA-Z0-9\\_\\-]+)*(\\:\\d{2,4})?(/?[a-zA-Z0-9\\-\\_\\.\\?\\=\\&\\%\\#]+)*/?)|([a-zA-Z0-9\\-\\_]+\\.)+([a-zA-Z\\-\\_]+)(\\:\\d{2,4})?(/?[a-zA-Z0-9\\-\\_\\.\\?\\=\\&\\%\\#]+)*/?|\\d+(\\.\\d+){3}(\\:\\d{2,4})?)\\)\n");
+//			Matcher matcherProtocol = patternProtocol.matcher(patternString);
+//			LogUtils.YfcDebug("取出来的文字"+stringBuilder.reverse());
+//			while (matcherProtocol.find()) {
+//				protocolResource = matcherProtocol.group();
+//				if(protocolResource.startsWith("(ecm-contact://")){
+//					protocol = protocolResource.replace("(", "").replace(")", "");
+//					protocolResource = protocolResource.replace("ecm-contact://", "")
+//							.replace("(", "\"").replace(")", "\"");
+//				}else {
+//					protocol = protocolResource.replace("(", "").replace(")", "");
+//				}
+//			}
+//			LogUtils.YfcDebug("protocol:"+protocol);
+//			LogUtils.YfcDebug("protocolResource:"+protocolResource);
 			Pattern patternContent = Pattern.compile("\\[.*\\]");
 			Matcher matcherContent = patternContent.matcher(patternString);
 			while (matcherContent.find()) {
