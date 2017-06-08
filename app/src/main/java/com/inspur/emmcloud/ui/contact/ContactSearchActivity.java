49fa84a6d5f311581df9c5a2c10909f893a74d09
Chen Mingchao <chenmch@inspur.com>
2017-06-08 16:42:55 +0800

    整理代码

--- app/src/main/java/com/inspur/emmcloud/ui/contact/ContactSearchActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/contact/ContactSearchActivity.java
@@ -1425,24 +1425,25 @@ public class ContactSearchActivity extends BaseActivity {
 	 * @param photoLayout
 	 */
 	private void displayImg(SearchModel searchModel, CircleFrameLayout photoLayout) {
-		String icon = searchModel.getIcon();
 		String type = searchModel.getType();
 		if (type.equals("GROUP")) {
 			DisplayChannelGroupIcon.show(ContactSearchActivity.this,searchModel.getId(),photoLayout);
 		} else {
+			int defaultIcon;
+			String icon = null;
 			View channelPhotoView = LayoutInflater.from(ContactSearchActivity.this).inflate(R.layout.chat_msg_session_photo_one, null);
 			ImageView photoImg = (ImageView) channelPhotoView.findViewById(R.id.photo_img1);
 			if (type.equals("STRUCT")){
-				new ImageDisplayUtils().display(photoImg,"drawable://"+R.drawable.icon_channel_group_default);
+				defaultIcon = R.drawable.icon_channel_group_default;
 			}else{
-				int defaultIcon = R.drawable.icon_person_default;
-				if (searchModel.getId().equals("null")) {
-					photoLayout.setBackgroundResource(defaultIcon);
-					return;
+				defaultIcon = R.drawable.icon_person_default;
+				if (!searchModel.getId().equals("null")) {
+					icon = searchModel.getIcon();
 				}
-				new ImageDisplayUtils(getApplicationContext(), defaultIcon).display(
-						photoImg, icon);
+
 			}
+			new ImageDisplayUtils(getApplicationContext(), defaultIcon).display(
+					photoImg, icon);
 
 			photoLayout.addView(channelPhotoView);
 		}
