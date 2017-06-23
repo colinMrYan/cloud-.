c1f7d307f05a51088bad6afa2b9404783bc476c2
Yu Fuchang <yufuchang@inspur.com>
2017-06-23 10:19:25 +0800

    华为推送添加类似JPush的逻辑

--- app/src/main/java/com/inspur/emmcloud/broadcastreceiver/HuaWeiPushReceiver.java
+++ app/src/main/java/com/inspur/emmcloud/broadcastreceiver/HuaWeiPushReceiver.java
@@ -2,10 +2,22 @@ package com.inspur.emmcloud.broadcastreceiver;
 
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.huawei.hms.support.api.push.PushReceiver;
+import com.inspur.emmcloud.bean.CalendarEvent;
+import com.inspur.emmcloud.ui.IndexActivity;
+import com.inspur.emmcloud.ui.login.LoginActivity;
+import com.inspur.emmcloud.ui.work.calendar.CalEventAddActivity;
 import com.inspur.emmcloud.util.LogUtils;
+import com.inspur.emmcloud.util.PreferencesUtils;
+import com.inspur.emmcloud.util.StringUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cn.jpush.android.api.JPushInterface;
 
 /**
  * Created by yufuchang on 2017/6/20.
@@ -71,7 +83,69 @@ public class HuaWeiPushReceiver extends PushReceiver{
                     (BOUND_KEY.pushMsgKey);
             LogUtils.YfcDebug(content);
         }
+        String accessToken = PreferencesUtils.getString(context,
+                "accessToken", "");
+        if(!StringUtils.isBlank(accessToken)){
+            String extra = "";
+            if (extras.containsKey(JPushInterface.EXTRA_EXTRA)) {
+                extra = extras.getString(JPushInterface.EXTRA_EXTRA);
+            }
+            if (!StringUtils.isBlank(extra)) {
+                try {
+                    JSONObject extraObj = new JSONObject(extra);
+                    if (extraObj.has("calEvent")) {
+                        String json = extraObj.getString("calEvent");
+                        JSONObject calEventObj = new JSONObject(json);
+                        openCalEvent(context, calEventObj);
+                        return;
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            startIndexActivity(context);
+        }else{
+            statLoginActivity(context);
+        }
         super.onEvent(context, event, extras);
+    }
+
+    /**
+     * 打开应用首页
+     * @param context
+     */
+    private void startIndexActivity(Context context) {
+        Intent indexLogin = new Intent(context, IndexActivity.class);
+        indexLogin.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        indexLogin.putExtra("command","open_notification");
+        context.startActivity(indexLogin);
+    }
+
+    /**
+     * 打开登录
+     * @param context
+     */
+    private void statLoginActivity(Context context) {
+        Intent loginIntent = new Intent(context, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(loginIntent);
+    }
+
+
+    /**
+     * 打开
+     * @param context
+     * @param jsonObject
+     */
+    private void openCalEvent(Context context, JSONObject jsonObject) {
+        // TODO Auto-generated method stub
+        CalendarEvent calendarEvent = new CalendarEvent(jsonObject);
+        Intent intent = new Intent();
+        intent.setClass(context, CalEventAddActivity.class);
+        intent.putExtra("calEvent", calendarEvent);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
     }
 
     /**
