28aaf85a0fcd596eaab88f2ab33d0461caacc04b
chenmch <chenmch@inspur.com>
2017-07-10 09:49:03 +0800

    暂存

---
+++ app/src/main/java/com/inspur/emmcloud/ui/SchemeActivity.java
@@ -0,0 +1,16 @@
+package com.inspur.emmcloud.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by chenmch on 2017/7/10.
+ */
+
+public class SchemeActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
