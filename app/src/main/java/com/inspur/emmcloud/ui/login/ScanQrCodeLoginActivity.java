1189e96c88e7b8015e1449dcf86db1a0ba6386a8
Yu Fuchang <yufuchang@inspur.com>
2017-07-28 08:45:56 +0800

    扫一扫登录桌面版新界面部分实现代码

---
+++ app/src/main/java/com/inspur/emmcloud/ui/login/ScanQrCodeLoginActivity.java
@@ -0,0 +1,26 @@
+package com.inspur.emmcloud.ui.login;
+
+import android.os.Bundle;
+
+import com.inspur.emmcloud.BaseActivity;
+
+
+/**
+ * Created by yufuchang on 2017/7/27.
+ */
+
+public class ScanQrCodeLoginActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    private void closeLoginInActivity() {
+        finish();
+    }
+
+    private void changeLoginUI(){
+
+    }
+}
