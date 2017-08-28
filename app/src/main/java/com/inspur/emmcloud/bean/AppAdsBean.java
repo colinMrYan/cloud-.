688bdebb3f83fe365e98fd9c3fe4eb7429b3c4ed
chenmch@inspur.com <cmc123456>
2017-08-28 11:08:12 +0800

    1.替换新闻显示默认图片 2.解决appAddbean报错的问题

--- app/src/main/java/com/inspur/emmcloud/bean/AppAdsBean.java
+++ app/src/main/java/com/inspur/emmcloud/bean/AppAdsBean.java
@@ -4,13 +4,12 @@ import com.inspur.emmcloud.util.JSONUtils;
 
 import org.json.JSONObject;
 
-import lombok.Data;
 
 /**
  * Created by yufuchang on 2017/8/21.
  */
 
-@Data
+
 public class AppAdsBean {
 
     /**
@@ -33,16 +32,81 @@ public class AppAdsBean {
     private int sort_order = 0;
     private String legend = "";
 
-    public AppAdsBean(){}
-    public AppAdsBean(JSONObject obj){
-        id = JSONUtils.getString(obj,"id","");
-        ads_name = JSONUtils.getString(obj,"ads_name","");
-        ads_loc_id = JSONUtils.getString(obj,"ads_loc_id","");
-        url = JSONUtils.getString(obj,"url","");
-        status = JSONUtils.getInt(obj,"status",0);
-        ads_category = JSONUtils.getString(obj,"ads_category","");
-        sort_order = JSONUtils.getInt(obj,"sort_order",0);
-        legend = JSONUtils.getString(obj,"legend","");
+    public AppAdsBean() {
     }
 
+    public AppAdsBean(JSONObject obj) {
+        id = JSONUtils.getString(obj, "id", "");
+        ads_name = JSONUtils.getString(obj, "ads_name", "");
+        ads_loc_id = JSONUtils.getString(obj, "ads_loc_id", "");
+        url = JSONUtils.getString(obj, "url", "");
+        status = JSONUtils.getInt(obj, "status", 0);
+        ads_category = JSONUtils.getString(obj, "ads_category", "");
+        sort_order = JSONUtils.getInt(obj, "sort_order", 0);
+        legend = JSONUtils.getString(obj, "legend", "");
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAds_name() {
+        return ads_name;
+    }
+
+    public void setAds_name(String ads_name) {
+        this.ads_name = ads_name;
+    }
+
+    public String getAds_loc_id() {
+        return ads_loc_id;
+    }
+
+    public void setAds_loc_id(String ads_loc_id) {
+        this.ads_loc_id = ads_loc_id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getAds_category() {
+        return ads_category;
+    }
+
+    public void setAds_category(String ads_category) {
+        this.ads_category = ads_category;
+    }
+
+    public int getSort_order() {
+        return sort_order;
+    }
+
+    public void setSort_order(int sort_order) {
+        this.sort_order = sort_order;
+    }
+
+    public String getLegend() {
+        return legend;
+    }
+
+    public void setLegend(String legend) {
+        this.legend = legend;
+    }
 }
