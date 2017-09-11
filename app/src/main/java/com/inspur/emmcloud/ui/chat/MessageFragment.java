f5dfcf6bac54431e919e68f0d05483a922925d08
chenmch <chenmch@inspur.com>
2017-09-11 19:33:58 +0800

    解决当点击通知打开手机解锁页面时无法正确弹出解锁页面的问题

--- app/src/main/java/com/inspur/emmcloud/ui/chat/MessageFragment.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/MessageFragment.java
@@ -118,7 +118,7 @@ public class MessageFragment extends Fragment implements OnRefreshListener {
     private TextView titleText;
     private boolean isHaveCreatGroupIcon = false;
     private PopupWindow popupWindow;
-    private boolean isFirstConnectWebsockt = true;//判断是否第一次连上websockt
+    //private boolean isFirstConnectWebsockt = true;//判断是否第一次连上websockt
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -1066,11 +1066,11 @@ public class MessageFragment extends Fragment implements OnRefreshListener {
         if (socketStatus.equals("socket_connecting")) {
             titleText.setText(R.string.socket_connecting);
         } else if (socketStatus.equals(Socket.EVENT_CONNECT)) {
-            //当断开以后连接成功(非第一次连接上)后重新拉取一遍消息
-            if (!isFirstConnectWebsockt){
-                getChannelContent();
-            }
-            isFirstConnectWebsockt = false;
+//            //当断开以后连接成功(非第一次连接上)后重新拉取一遍消息
+//            if (!isFirstConnectWebsockt){
+//                getChannelContent();
+//            }
+//            isFirstConnectWebsockt = false;
             String appTabs = PreferencesByUserAndTanentUtils.getString(getActivity(), "app_tabbar_info_current", "");
             if (!StringUtils.isBlank(appTabs)) {
                 titleText.setText(AppTitleUtils.getTabTitle(getActivity(), getClass().getSimpleName()));
