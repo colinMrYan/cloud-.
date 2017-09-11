130cda11f23302f39b4eebe27b915a43a82a0d23
chenmch <chenmch@inspur.com>
2017-09-11 10:45:21 +0800

    填入微信开发者注册的AppKey和AppSecret

--- app/src/main/java/com/inspur/emmcloud/ui/mine/setting/RecommendAppActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/mine/setting/RecommendAppActivity.java
@@ -49,7 +49,7 @@ public class RecommendAppActivity extends BaseActivity {
         Config.DEBUG = true;
         QueuedWork.isUseThreadPool = false;
         UMShareAPI.get(this);
-        PlatformConfig.setWeixin("xxxxxx", "xxxxxx");
+        PlatformConfig.setWeixin("wx4eb8727ea9c26495", "56a0426315f1d0985a1cc1e75e96130d");
         PlatformConfig.setQQZone("1105561850", "1kaw4r1c37SUupFL");
     }
 
