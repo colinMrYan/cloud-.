344c4a40719f90d26243007360f8d85384e61763
Yu Fuchang <yufuchang@inspur.com>
2017-09-12 19:43:02 +0800

    缓存工具类测试

--- app/src/main/java/com/inspur/emmcloud/util/MyAppCacheUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/MyAppCacheUtils.java
@@ -3,8 +3,13 @@ package com.inspur.emmcloud.util;
 import android.content.Context;
 
 import com.alibaba.fastjson.JSON;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.inspur.emmcloud.bean.AppGroupBean;
+import com.inspur.emmcloud.config.MyAppConfig;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
 
 /**
@@ -19,10 +24,17 @@ public class MyAppCacheUtils {
      * @param appGroupList
      */
     public static void saveMyAppList(Context context, List<AppGroupBean> appGroupList){
-        String appList = JSON.toJSONString(appGroupList);
-        if(!appList.equals("null") && !StringUtils.isBlank(appList)){
-            PreferencesByUserAndTanentUtils.putString(context,"my_app_list",appList);
+        Gson gson=new Gson();
+        String gsonString = gson.toJson(appGroupList);
+//        String appList = JSON.toJSONString(appGroupList);
+        LogUtils.YfcDebug("存储的应用文字信息："+appGroupList.get(2).getAppItemList().get(0).toString());
+        writeData2File(gsonString,"before.txt");
+        if(!gsonString.equals("null") && !StringUtils.isBlank(gsonString)){
+            PreferencesByUserAndTanentUtils.putString(context,"my_app_list",gsonString);
         }
+
+
+
     }
 
     /**
@@ -40,6 +52,10 @@ public class MyAppCacheUtils {
      */
     public static List<AppGroupBean> getMyApps(Context context){
         String appsString = PreferencesByUserAndTanentUtils.getString(context,"my_app_list","");
+        Gson gson = new Gson();
+//        gson.fromJson(appsString,AppGroupBean.class);
+        List<AppGroupBean> appGroupBeenList = gson.fromJson(appsString, new TypeToken<List<AppGroupBean>>(){}.getType());
+        writeData2File(appsString,"after.txt");
         return JSON.parseArray(appsString,AppGroupBean.class);
     }
 
@@ -58,5 +74,70 @@ public class MyAppCacheUtils {
      */
     public  static boolean getHasCommonlyApp(Context context){
         return PreferencesByUserAndTanentUtils.getBoolean(context,"is_has_commonly_app",false);
+    }
+
+
+    private static void writeData2File(String content,String filename) {
+        String filePath = MyAppConfig.LOCAL_CACHE_PATH;
+        String fileName = filename;
+
+        writeTxtToFile(content, filePath, fileName);
+    }
+
+    // 将字符串写入到文本文件中
+    public static void writeTxtToFile(String strcontent, String filePath, String fileName) {
+        //生成文件夹之后，再生成文件，不然会出错
+        makeFilePath(filePath, fileName);
+
+        String strFilePath = filePath+fileName;
+        // 每次写入时，都换行写
+        String strContent = strcontent + "\r\n";
+        try {
+            File file = new File(strFilePath);
+            if (!file.exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+//            RandomAccessFile raf = new RandomAccessFile(file, "rwd");
+//            raf.seek(file.length());
+//            raf.write(strContent.getBytes());
+//            raf.close();
+
+
+            FileOutputStream out = null;
+            out = new FileOutputStream(file);
+            out.write(strContent.getBytes());
+            out.flush();
+        } catch (Exception e) {
+            LogUtils.YfcDebug("Error on write File:" + e);
+        }
+    }
+
+    // 生成文件
+    public static File makeFilePath(String filePath, String fileName) {
+        File file = null;
+        makeRootDirectory(filePath);
+        try {
+            file = new File(filePath + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    // 生成文件夹
+    public static void makeRootDirectory(String filePath) {
+        File file = null;
+        try {
+            file = new File(filePath);
+            if (!file.exists()) {
+                file.mkdir();
+            }
+        } catch (Exception e) {
+            LogUtils.YfcDebug( e+"");
+        }
     }
 }
