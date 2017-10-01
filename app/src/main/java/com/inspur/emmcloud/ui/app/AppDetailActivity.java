a560b2f41f68a90c3a0ad84f067f59b596749ee4
Yu Fuchang <yufuchang@inspur.com>
2017-10-01 14:26:37 +0800

    通过改变控件修复应用详情中图片第一次进入变形问题

--- app/src/main/java/com/inspur/emmcloud/ui/app/AppDetailActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/app/AppDetailActivity.java
@@ -1,13 +1,13 @@
 package com.inspur.emmcloud.ui.app;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,16 +21,15 @@ import com.inspur.emmcloud.bean.App;
 import com.inspur.emmcloud.bean.GetAddAppResult;
 import com.inspur.emmcloud.util.AppCenterNativeAppUtils;
 import com.inspur.emmcloud.util.ImageDisplayUtils;
-import com.inspur.emmcloud.util.LogUtils;
 import com.inspur.emmcloud.util.NetUtils;
 import com.inspur.emmcloud.util.StringUtils;
 import com.inspur.emmcloud.util.UriUtils;
 import com.inspur.emmcloud.util.WebServiceMiddleUtils;
-import com.inspur.emmcloud.widget.HorizontalListView;
 import com.inspur.emmcloud.widget.LoadingDialog;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -40,17 +39,15 @@ import java.util.List;
  */
 public class AppDetailActivity extends BaseActivity {
 
-    private static final int ADD_APP_FAIL = 3;
     private static final String ACTION_NAME = "add_app";
     private ImageView appIconImg;
     private Button statusBtn;
-    private HorizontalListView intrImgListView;
+    private RecyclerView intrImgListView;
     private ImageDisplayUtils imageDisplayUtils;
     private LoadingDialog loadingDlg;
     private MyAppAPIService apiService;
     private ReactNativeAPIService reactNativeApiService;
     private App app;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,57 +56,114 @@ public class AppDetailActivity extends BaseActivity {
         setContentView(R.layout.activity_app_detail);
         imageDisplayUtils = new ImageDisplayUtils(R.drawable.icon_empty_icon);
         loadingDlg = new LoadingDialog(AppDetailActivity.this);
-        app = (App) getIntent().getExtras().getSerializable("app");
-//		initView();
+        String appId = ((App) getIntent().getExtras().getSerializable("app")).getAppID();
         apiService = new MyAppAPIService(this);
         apiService.setAPIInterface(new WebService());
         reactNativeApiService = new ReactNativeAPIService(AppDetailActivity.this);
         reactNativeApiService.setAPIInterface(new WebService());
-        getAppInfoById();
+        getAppInfoById(appId);
     }
 
-    private void initView(App app) {
+    private void initView(final App app) {
         // TODO Auto-generated method stub
         appIconImg = (ImageView) findViewById(R.id.app_icon_img);
         imageDisplayUtils.displayImage(appIconImg, app.getAppIcon());
         statusBtn = (Button) findViewById(R.id.app_status_btn);
-        intrImgListView = (HorizontalListView) findViewById(R.id.intr_img_list);
-        intrImgListView.setAdapter(new Adapter(app.getLegends()));
-        intrImgListView.setOnItemClickListener(new OnItemClickListener() {
-
+        intrImgListView = (RecyclerView) findViewById(R.id.intr_app_img_list);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(AppDetailActivity.this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        intrImgListView.setLayoutManager(linearLayoutManager);
+        AppDetailImageAdapter appDetailImageAdapter = new AppDetailImageAdapter(AppDetailActivity.this,app.getLegends());
+        appDetailImageAdapter.setOnRecommandItemClickListener(new OnAppDetailImageItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                // TODO Auto-generated method stub
-//					Intent intent = new Intent();
-//					intent.setClass(getApplicationContext(), AppImgDisPlayActivity.class);
-//					intent.putExtra("currentIndex", position);
-//					startActivity(intent);
+            public void onAppDetailImageItemClick(View view, int position) {
+                Intent intent = new Intent();
+					intent.setClass(getApplicationContext(), AppImgDisPlayActivity.class);
+					intent.putExtra("currentIndex", position);
+                    intent.putStringArrayListExtra("legends", (ArrayList<String>) app.getLegends());
+					startActivity(intent);
             }
         });
+        intrImgListView.setAdapter(appDetailImageAdapter);
         ((TextView) findViewById(R.id.name_text)).setText(app.getAppName());
         ((TextView) findViewById(R.id.profile_text)).setText(app.getNote());
         if (!StringUtils.isBlank(app.getDescription())) {
             ((TextView) findViewById(R.id.intr_text)).setText(app
                     .getDescription());
         }
-        showAppStatusBtn();
+        showAppStatusBtn(app);
+    }
+
+    /**
+     * 推荐应用的Adapter
+     */
+    public class AppDetailImageAdapter extends RecyclerView.Adapter<AppDetailImageAdapter.AppDetailImageViewHolder> {
+        private LayoutInflater inflater;
+        private OnAppDetailImageItemClickListener onAppDetailImageItemClickListener;
+        private List<String> legends;
+        private ImageDisplayUtils imageDisplayUtils;
+        public AppDetailImageAdapter(Context context,List<String> legends) {
+            inflater = LayoutInflater.from(context);
+            imageDisplayUtils = new ImageDisplayUtils();
+            this.legends = legends;
+        }
+
+        @Override
+        public AppDetailImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = inflater.inflate(R.layout.app_intr_img_view, null);
+            AppDetailImageViewHolder viewHolder = new AppDetailImageViewHolder(view);
+            viewHolder.appDetailImg = (ImageView) view.findViewById(R.id.detail_img);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(final AppDetailImageViewHolder holder, final int position) {
+            imageDisplayUtils.displayImage(holder.appDetailImg,legends.get(position));
+            if (onAppDetailImageItemClickListener != null) {
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onAppDetailImageItemClickListener.onAppDetailImageItemClick(holder.itemView, position);
+                    }
+                });
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return legends.size();
+        }
+
+        public void setOnRecommandItemClickListener(OnAppDetailImageItemClickListener l) {
+            this.onAppDetailImageItemClickListener = l;
+        }
+
+        public class AppDetailImageViewHolder extends RecyclerView.ViewHolder {
+            ImageView appDetailImg;
+            public AppDetailImageViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+    }
+
+    public interface OnAppDetailImageItemClickListener {
+        void onAppDetailImageItemClick(View view, int position);
     }
 
     /**
      * 根据id获取app详情
      */
-    private void getAppInfoById() {
+    private void getAppInfoById(String appId) {
         if (NetUtils.isNetworkConnected(AppDetailActivity.this)) {
             loadingDlg.show();
-            apiService.getAppInfo(app.getAppID());
+            apiService.getAppInfo(appId);
         }
     }
 
     /**
      * 显示app状态的按钮
      */
-    private void showAppStatusBtn() {
+    private void showAppStatusBtn(App app) {
         if (app.getUseStatus() == 1) {
             statusBtn.setText(getString(R.string.open));
         } else if (app.getUseStatus() == 0) {
@@ -132,10 +186,8 @@ public class AppDetailActivity extends BaseActivity {
         }
     }
 
-
     /**
      * 封装网络请求的方法
-     *
      * @param statusBtn
      * @param appID
      */
@@ -151,57 +203,14 @@ public class AppDetailActivity extends BaseActivity {
         finish();
     }
 
-    private class Adapter extends BaseAdapter {
-        public List<String> legends;
-
-        public Adapter(List<String> legends) {
-            // TODO Auto-generated constructor stub
-            this.legends = legends;
-        }
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return legends.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            // TODO Auto-generated method stub
-            return null;
-        }
-
-        @Override
-        public long getItemId(int position) {
-            // TODO Auto-generated method stub
-            return 0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-            LayoutInflater vi = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-            convertView = vi.inflate(R.layout.app__intr_img_view, null);
-            ImageView image = (ImageView) convertView.findViewById(R.id.detail_img);
-            imageDisplayUtils.displayImage(image, legends.get(position));
-            return convertView;
-        }
-
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        LogUtils.jasonDebug("requestCode=" + requestCode);
-        LogUtils.jasonDebug("resultCode=" + resultCode);
-        LogUtils.jasonDebug("data=" + data.getDataString());
         super.onActivityResult(requestCode, resultCode, data);
     }
 
     public class WebService extends APIInterfaceInstance {
-
         @Override
         public void returnAddAppSuccess(GetAddAppResult getAddAppResult) {
-            // TODO Auto-generated method stub
             if (loadingDlg != null && loadingDlg.isShowing()) {
                 loadingDlg.dismiss();
             }
@@ -215,7 +224,6 @@ public class AppDetailActivity extends BaseActivity {
 
         @Override
         public void returnAddAppFail(String error, int errorCode) {
-            // TODO Auto-generated method stub
             if (loadingDlg != null && loadingDlg.isShowing()) {
                 loadingDlg.dismiss();
             }
@@ -229,6 +237,7 @@ public class AppDetailActivity extends BaseActivity {
             if (loadingDlg != null && loadingDlg.isShowing()) {
                 loadingDlg.dismiss();
             }
+            AppDetailActivity.this.app = app;
             initView(app);
         }
 
@@ -237,14 +246,9 @@ public class AppDetailActivity extends BaseActivity {
             if (loadingDlg != null && loadingDlg.isShowing()) {
                 loadingDlg.dismiss();
             }
+            app = (App) getIntent().getExtras().getSerializable("app");
             WebServiceMiddleUtils.hand(AppDetailActivity.this, error, errorCode);
         }
-    }
-
-    @Override
-    public void finish() {
-        // TODO Auto-generated method stub
-        super.finish();
     }
 
 }
