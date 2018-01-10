c539edcec453a22df9f0471e0b1c6ae055753e04
chenmch <chenmch@inspur.com>
2018-01-10 15:08:15 +0800

    解决有的手机扫码很慢的问题

---
+++ app/src/main/java/com/inspur/imp/plugin/barcode/decoding/CaptureActivityHandler.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (C) 2008 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.inspur.imp.plugin.barcode.decoding;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+import com.inspur.imp.api.Res;
+import com.inspur.imp.plugin.barcode.camera.CameraManager;
+import com.inspur.imp.plugin.barcode.scan.BarCodeService;
+import com.inspur.imp.plugin.barcode.scan.CaptureActivity;
+import com.inspur.imp.plugin.barcode.view.ViewfinderResultPointCallback;
+
+import java.util.Vector;
+
+
+/**
+ * This class handles all the messaging which comprises the state machine for capture.
+ *
+ * @author dswitkin@google.com (Daniel Switkin)
+ */
+public final class CaptureActivityHandler extends Handler {
+
+  private static final String TAG = CaptureActivityHandler.class.getSimpleName();
+
+  private final CaptureActivity activity;
+  private final DecodeThread decodeThread;
+  private State state;
+
+  private enum State {
+    PREVIEW,
+    SUCCESS,
+    DONE
+  }
+
+  public CaptureActivityHandler(CaptureActivity activity, Vector<BarcodeFormat> decodeFormats,
+                                String characterSet) {
+    this.activity = activity;
+    decodeThread = new DecodeThread(activity, decodeFormats, characterSet,
+        new ViewfinderResultPointCallback(activity.getViewfinderView()));
+    decodeThread.start();
+    state = State.SUCCESS;
+
+    // Start ourselves capturing previews and decoding.
+    CameraManager.get().startPreview();
+    restartPreviewAndDecode();
+  }
+
+  @Override
+  public void handleMessage(Message message) {
+	  if (Res.getWidgetID("auto_focus") == message.what) {
+		  if (state == State.PREVIEW) {
+	          CameraManager.get().requestAutoFocus(this, Res.getWidgetID("auto_focus"));
+	        }
+		} else if (Res.getWidgetID("restart_preview") == message.what) {
+			Log.d(TAG, "Got restart preview message");
+	        restartPreviewAndDecode();
+		} else if (Res.getWidgetID("decode_succeeded") == message.what) {
+			 Log.d(TAG, "Got decode succeeded message");
+	        state = State.SUCCESS;
+	        Bundle bundle = message.getData();
+	        Bitmap barcode = bundle == null ? null :
+	            (Bitmap) bundle.getParcelable(DecodeThread.BARCODE_BITMAP);
+	        String str_result=((Result) message.obj).getText();
+	        String functName = BarCodeService.functName;
+			// 返回结果的回调函数
+			//将内容区域的回车换行去除
+	        str_result = str_result.replaceAll("[\\t\\n\\r]", "");
+	        activity.handDecodeResult(str_result);
+//			BarCodeService.barcodeService.jsCallback(functName, str_result.toString());
+//			activity.finish();
+		} else if (Res.getWidgetID("decode_failed") == message.what) {
+			// We're decoding as fast as possible, so when one decode fails, start another.
+	        state = State.PREVIEW;
+	        CameraManager.get().requestPreviewFrame(decodeThread.getHandler(), Res.getWidgetID("decode"));
+		} else if (Res.getWidgetID("return_scan_result") == message.what) {
+			 Log.d(TAG, "Got return scan result message");
+	        activity.setResult(Activity.RESULT_OK, (Intent) message.obj);
+	        activity.finish();
+		} 
+  }
+
+  public void quitSynchronously() {
+    state = State.DONE;
+    CameraManager.get().stopPreview();
+    Message quit = Message.obtain(decodeThread.getHandler(),Res.getWidgetID("quit"));
+    quit.sendToTarget();
+    try {
+      decodeThread.join();
+    } catch (InterruptedException e) {
+      // continue
+    }
+
+    // Be absolutely sure we don't send any queued up messages
+    removeMessages(Res.getWidgetID("decode_succeeded"));
+    //removeMessages(R.id.return_scan_result);
+    removeMessages(Res.getWidgetID("decode_failed"));
+  }
+
+  private void restartPreviewAndDecode() {
+    if (state == State.SUCCESS) {
+      state = State.PREVIEW;
+      CameraManager.get().requestPreviewFrame(decodeThread.getHandler(), Res.getWidgetID("decode"));
+      CameraManager.get().requestAutoFocus(this, Res.getWidgetID("auto_focus"));
+      activity.drawViewfinder();
+    }
+  }
+
+}
