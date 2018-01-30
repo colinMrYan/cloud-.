1492bb7465b9b89a15326a8dc9dee312e37d42cf
Yu Fuchang <yufuchang@inspur.com>
2018-01-30 10:39:23 +0800

    暂存

---
+++ app/src/main/java/com/inspur/emmcloud/ui/login/LoginMoreActivity.java
@@ -0,0 +1,16 @@
+package com.inspur.emmcloud.ui.login;
+
+import android.os.Bundle;
+
+import com.inspur.emmcloud.BaseActivity;
+
+/**
+ * Created by yufuchang on 2018/1/30.
+ */
+
+public class LoginMoreActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
