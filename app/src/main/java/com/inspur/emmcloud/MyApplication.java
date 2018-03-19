5d5a598d5f4b55140cc6e6cfb7107b688005544a
chenmch <chenmch@inspur.com>
2018-03-19 13:25:06 +0800

    解决token失效后换token退出登录的问题

--- app/src/main/java/com/inspur/emmcloud/MyApplication.java
+++ app/src/main/java/com/inspur/emmcloud/MyApplication.java
@@ -643,6 +643,10 @@ public class MyApplication extends MultiDexApplication implements ReactApplicati
 
     }
 
+    public MyActivityLifecycleCallbacks getActivityLifecycleCallbacks(){
+        return myActivityLifecycleCallbacks;
+    }
+
 
     /**
      * init ImageLoader
