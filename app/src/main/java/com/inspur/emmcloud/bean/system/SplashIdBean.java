9a9f494a194345575b9adc84c9a0b6735b42c7a9
Yu Fuchang <yufuchang@inspur.com>
2018-04-02 11:57:11 +0800

    改造Splash实体

---
+++ app/src/main/java/com/inspur/emmcloud/bean/system/SplashIdBean.java
@@ -0,0 +1,49 @@
+package com.inspur.emmcloud.bean.system;
+
+import com.inspur.emmcloud.util.common.JSONUtils;
+
+/**
+ * Created by yufuchang on 2018/4/2.
+ */
+
+public  class SplashIdBean {
+    /**
+     * namespace : com.inspur.ecc.core.preferences
+     * domain : launch-screen
+     * version : v1.0.0
+     */
+    private String namespace = "";
+    private String domain = "";
+    private String version = "";
+
+    public SplashIdBean(String response){
+        this.namespace = JSONUtils.getString(response,"namespace","");
+        this.domain = JSONUtils.getString(response,"domain","");
+        this.version = JSONUtils.getString(response,"version","");
+
+    }
+
+    public String getNamespace() {
+        return namespace;
+    }
+
+    public void setNamespace(String namespace) {
+        this.namespace = namespace;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
