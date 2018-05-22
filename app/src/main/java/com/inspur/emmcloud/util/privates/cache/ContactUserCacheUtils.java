21b3e3a212135124ce16cb853505cb58fec5b95c
chenmch <chenmch@inspur.com>
2018-05-22 17:28:25 +0800

    暂存

--- app/src/main/java/com/inspur/emmcloud/util/privates/cache/ContactUserCacheUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/privates/cache/ContactUserCacheUtils.java
@@ -33,4 +33,94 @@ public class ContactUserCacheUtils {
         return  PreferencesByUserAndTanentUtils.getLong(MyApplication.getInstance(), Constant.PREF_CONTACT_USER_LASTQUERYTIME,0L);
     }
 
+    public static String getUserName(String uid){
+        String userName = "";
+        try {
+            ContactUser contactUser = DbCacheUtils.getDb().findById(ContactUser.class,uid);
+            if (contactUser != null){
+                userName = contactUser.getName();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return userName;
+    }
+
+
+        /**
+     * 通过用户名获取Contact
+     * @param userName
+     * @return
+     */
+    public static ContactUser getContactUser(String userName){
+        ContactUser contactUser = null;
+        try {
+            contactUser = DbCacheUtils.getDb().selector(ContactUser.class).where(
+                    "name", "=", userName).findFirst();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return contactUser;
+    }
+
+
+    //    /**
+//     * 通过id List获取PersonDto对象的List
+//     *
+//     * @param context
+//     * @param uidList
+//     * @return
+//     */
+//    public static List<PersonDto> getShowMemberList(Context context, List<String> uidList) {
+//        List<Contact> userList = new ArrayList<Contact>();
+//        List<Robot> robotList = new ArrayList<>();
+//        List<PersonDto> unitMemberList = new ArrayList<>();
+//        try {
+//            userList = DbCacheUtils.getDb(context).selector(Contact.class).where("inspurID",
+//                    "in", uidList).findAll();
+//            robotList = DbCacheUtils.getDb(context).selector(Robot.class).where("id",
+//                    "in", uidList).findAll();
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//            e.printStackTrace();
+//
+//        }
+//
+//        if (userList != null) {
+//            Iterator<Contact> contactListIterator = userList.iterator();
+//            while (contactListIterator.hasNext()) {
+//                Contact contact = contactListIterator.next();
+//                PersonDto personDto = new PersonDto();
+//                personDto.setName(contact.getRealName());
+//                personDto.setUid(contact.getInspurID());
+//                personDto.setSortLetters(contact.getPinyin().substring(0, 1));
+//                personDto.setPinyinFull(contact.getPinyin());
+//                personDto.setSuoxie(PinyinUtils.getPinYinHeadChar(contact
+//                        .getRealName()));
+//                personDto.setUtype("contact");
+//                unitMemberList.add(personDto);
+//            }
+//        }
+//
+//        if (robotList != null) {
+//            Iterator<Robot> robotListIterator = robotList.iterator();
+//            while (robotListIterator.hasNext()) {
+//                Robot robot = robotListIterator.next();
+//                PersonDto personDto = new PersonDto();
+//                personDto.setName(robot.getName());
+//                personDto.setUid(robot.getId());
+//                personDto.setSortLetters(PinyinUtils.getPingYin(robot.getName()).substring(0, 1));
+//                personDto.setPinyinFull(PinyinUtils.getPingYin(robot.getName()));
+//                personDto.setSuoxie(PinyinUtils.getPinYinHeadChar(robot.getName()));
+//                personDto.setUtype("robot");
+//                unitMemberList.add(personDto);
+//            }
+//        }
+//
+//        if (unitMemberList == null) {
+//            unitMemberList = new ArrayList<PersonDto>();
+//        }
+//        return unitMemberList;
+//
+//    }
 }
