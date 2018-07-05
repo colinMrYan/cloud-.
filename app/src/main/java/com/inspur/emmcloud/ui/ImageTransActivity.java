84440465bd4a39b9f70a6cc6631c0060633925e6
chenmch <chenmch@inspur.com>
2018-07-05 14:40:45 +0800

    暂存

---
+++ app/src/main/java/com/inspur/emmcloud/ui/ImageTransActivity.java
@@ -0,0 +1,10 @@
+package com.inspur.emmcloud.ui;
+
+import com.inspur.emmcloud.BaseActivity;
+
+/**
+ * Created by chenmch on 2018/7/5.
+ */
+
+public class ImageTransActivity extends BaseActivity {
+}
