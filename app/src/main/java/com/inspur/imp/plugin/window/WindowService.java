c20ad3199c0713efbb825606d7c813d97f7f92e0
chenmch <chenmch@inspur.com>
2018-08-02 15:15:56 +0800

    暂存

--- app/src/main/java/com/inspur/imp/plugin/window/WindowService.java
+++ app/src/main/java/com/inspur/imp/plugin/window/WindowService.java
@@ -4,7 +4,11 @@ import com.inspur.emmcloud.util.common.JSONUtils;
 import com.inspur.emmcloud.util.privates.UriUtils;
 import com.inspur.imp.plugin.ImpPlugin;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yufuchang on 2018/7/20.
@@ -14,9 +18,19 @@ public class WindowService extends ImpPlugin {
     @Override
     public void execute(String action, JSONObject paramsObject) {
         if ("open".equals(action)) {
-            UriUtils.openUrl(getActivity(), JSONUtils.getString(paramsObject,"url",""),JSONUtils.getString(paramsObject,"title",""));
-        }else{
+            UriUtils.openUrl(getActivity(), JSONUtils.getString(paramsObject, "url", ""), JSONUtils.getString(paramsObject, "title", ""));
+        } else if ("setTitles".equals(action)) {
+            showDropTitle(paramsObject);
+        } else {
             showCallIMPMethodErrorDlg();
+        }
+    }
+
+    private void showDropTitle(JSONObject paramsObject){
+        List<DropItemTitle> dropItemTitleList = new ArrayList<>();
+        JSONArray array = JSONUtils.getJSONArray(paramsObject,"config",new JSONArray());
+        for (int i=0;i<array.length();i++){
+            dropItemTitleList.add(new DropItemTitle(JSONUtils.getJSONObject(array,i,new JSONObject())));
         }
     }
 
