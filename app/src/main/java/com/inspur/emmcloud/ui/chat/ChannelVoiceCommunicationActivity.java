f9581389c268dc75052c3e25595da2461e75714a
Yu Fuchang <yufuchang@inspur.com>
2018-08-18 14:34:47 +0800

    UI控制逻辑暂存

--- app/src/main/java/com/inspur/emmcloud/ui/chat/ChannelVoiceCommunicationActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/ChannelVoiceCommunicationActivity.java
@@ -2,9 +2,14 @@ package com.inspur.emmcloud.ui.chat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Chronometer;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.inspur.emmcloud.BaseActivity;
 import com.inspur.emmcloud.R;
@@ -17,6 +22,7 @@ import com.inspur.emmcloud.util.common.LogUtils;
 import com.inspur.emmcloud.util.common.StateBarUtils;
 import com.inspur.emmcloud.util.common.ToastUtils;
 import com.inspur.emmcloud.util.privates.VoiceCommunicationUtils;
+import com.inspur.emmcloud.widget.CircleTextImageView;
 import com.inspur.emmcloud.widget.ECMSpaceItemDecoration;
 
 import org.xutils.view.annotation.ContentView;
@@ -31,13 +37,52 @@ import java.util.List;
  */
 @ContentView(R.layout.activity_voice_channel)
 public class ChannelVoiceCommunicationActivity extends BaseActivity{
+    public static final int INVITER_LAYOUT_STATE = 0;
+    public static final int INVITEE_LAYOUT_STATE = 1;
+    public static final int COMMUNICATION_STATE = 2;
     private VoiceCommunicationUtils voiceCommunicationUtils;
     private List<SearchModel> selectMemList = new ArrayList<SearchModel>();
     private static final int CHOOSE_MEMBERS = 1;
+    @ViewInject(R.id.ll_voice_communication_invite)
+    private LinearLayout linearLayoutInviter;
+    @ViewInject(R.id.img_user_head)
+    private CircleTextImageView imgUserHead;
+    @ViewInject(R.id.tv_user_name)
+    private TextView tvUserName;
+    @ViewInject(R.id.ll_voice_communication_invite_members)
+    private LinearLayout linearLayoutInviteMemebersGroup;
     @ViewInject(R.id.recyclerview_voice_communication_first)
     private RecyclerView recyclerViewFirst;
     @ViewInject(R.id.recyclerview_voice_communication_second)
     private RecyclerView recyclerViewSecond;
+    @ViewInject(R.id.ll_voice_communication_memebers)
+    private LinearLayout linearLayoutCommunicationMembers;
+    @ViewInject(R.id.recyclerview_voice_communication_memebers)
+    private RecyclerView recyclerViewCommunicationMembers;
+    @ViewInject(R.id.tv_voice_communication_state)
+    private TextView tvCommunicationState;
+    @ViewInject(R.id.tv_voice_communication_time)
+    private Chronometer chronometerCommunicationTime;
+    @ViewInject(R.id.ll_voice_communication_function_group)
+    private LinearLayout linearLayoutFunction;
+    @ViewInject(R.id.img_an_excuse)
+    private ImageView imgExcuse;
+    @ViewInject(R.id.tv_an_excuse)
+    private TextView tvExcuse;
+    @ViewInject(R.id.img_hands_free)
+    private ImageView imgHandsFree;
+    @ViewInject(R.id.tv_hands_free)
+    private TextView tvHandsFree;
+    @ViewInject(R.id.img_tran_video)
+    private ImageView imgTranVideo;
+    @ViewInject(R.id.tv_tran_video)
+    private TextView tvTranVideo;
+    @ViewInject(R.id.rl_voice_communication_operate)
+    private LinearLayout linearLayoutInviteOperate;
+    @ViewInject(R.id.img_answer_the_phone)
+    private ImageView imgAnswerPhone;
+    @ViewInject(R.id.img_hung_up)
+    private ImageView imgHungUp;
     private VoiceCommunicationMemberAdapter voiceCommunicationMemberAdapter;
     private List<VoiceCommunicationJoinChannelInfoBean> voiceCommunicationJoinChannelInfoBeanList = new ArrayList<>();
     @Override
@@ -63,7 +108,27 @@ public class ChannelVoiceCommunicationActivity extends BaseActivity{
         layoutManager2.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerViewSecond.setLayoutManager(layoutManager2);
         recyclerViewSecond.addItemDecoration(new ECMSpaceItemDecoration(DensityUtil.dip2px(this,8)));
+        GridLayoutManager layoutManagerMemebers = new GridLayoutManager(this,5);
+        recyclerViewCommunicationMembers.setLayoutManager(layoutManagerMemebers);
+        recyclerViewCommunicationMembers.addItemDecoration(new ECMSpaceItemDecoration(DensityUtil.dip2px(this,8)));
+        chronometerCommunicationTime.start();
+
+        initViewsVisibility(INVITEE_LAYOUT_STATE);
+
     }
+
+    /**
+     * 根据状态改变布局可见性
+     * @param state
+     */
+    private void initViewsVisibility(int state) {
+        linearLayoutInviter.setVisibility(View.VISIBLE);
+        linearLayoutInviteMemebersGroup.setVisibility(View.VISIBLE);
+        linearLayoutCommunicationMembers.setVisibility(View.VISIBLE);
+        linearLayoutFunction.setVisibility(View.VISIBLE);
+        linearLayoutInviteOperate.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
