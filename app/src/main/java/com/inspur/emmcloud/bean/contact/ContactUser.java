c523386adf9e0051f4076d9c79239f22a990946f
chenmch <chenmch@inspur.com>
2018-08-28 19:05:36 +0800

    通讯录增加工号

--- app/src/main/java/com/inspur/emmcloud/bean/contact/ContactUser.java
+++ app/src/main/java/com/inspur/emmcloud/bean/contact/ContactUser.java
@@ -17,7 +17,7 @@ import java.util.List;
  * Created by chenmch on 2018/5/10.
  */
 
-@Table(name="ContactUser")
+@Table(name="ContactUser",onCreated = "CREATE INDEX contactUserIndex ON ContactUser(id)")
 public class ContactUser {
     @Column(name = "id", isId = true)
     private String id="";
@@ -37,6 +37,8 @@ public class ContactUser {
     private int hasHead = 0;
     @Column(name = "sortOrder")
     private int sortOrder= 0;
+    @Column(name = "employeeNum")
+    private String employeeNum="";
     @Column(name = "lastQueryTime")
     private String lastQueryTime = "";
     public ContactUser(){
@@ -57,6 +59,7 @@ public class ContactUser {
         this.email = JSONUtils.getString(object,"email","");
         this.hasHead =JSONUtils.getInt(object,"has_head",0);
         this.sortOrder = JSONUtils.getInt(object,"sort_order",0);
+        this.employeeNum = JSONUtils.getString(object,"emp_no","");
         this.lastQueryTime = lastQueryTime;
     }
 
@@ -161,6 +164,14 @@ public class ContactUser {
 
     public String getLastQueryTime() {
         return lastQueryTime;
+    }
+
+    public String getEmployeeNum() {
+        return employeeNum;
+    }
+
+    public void setEmployeeNum(String employeeNum) {
+        this.employeeNum = employeeNum;
     }
 
     public void setLastQueryTime(String lastQueryTime) {
