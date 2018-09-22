b1a5e5937d44532623e4c87948d73aa254576243
Yu Fuchang <yufuchang@inspur.com>
2018-09-22 14:42:21 +0800

    修复语音bug

--- app/src/main/java/com/inspur/emmcloud/widget/audiorecord/AudioRecorderManager.java
+++ app/src/main/java/com/inspur/emmcloud/widget/audiorecord/AudioRecorderManager.java
@@ -73,7 +73,7 @@ public class AudioRecorderManager {
             // 开启音频文件写入线程
             new Thread(new AudioRecordThread()).start();
         }catch (Exception e){
-            callBack.onAudioPrepareError();
+            callBack.onWavAudioPrepareState(AudioRecordErrorCode.E_ERROR);
             e.printStackTrace();
         }
 
@@ -83,19 +83,21 @@ public class AudioRecorderManager {
      * 准备
      * @return
      */
-    public void prepareAudioRecord(){
+    public void prepareWavAudioRecord(){
         //判断是否有外部存储设备sdcard
         if (isSdcardExit()) {
             if (isRecording) {
-                callBack.onAudioPrepared(AudioRecordErrorCode.E_STATE_RECODING);
+                callBack.onWavAudioPrepareState(AudioRecordErrorCode.E_STATE_RECODING);
+                return;
             } else {
                 if (audioRecord == null) {
                     createAudioRecord();
                 }
-                callBack.onAudioPrepared(AudioRecordErrorCode.SUCCESS);
+                callBack.onWavAudioPrepareState(AudioRecordErrorCode.SUCCESS);
             }
         } else {
-            callBack.onAudioPrepared(AudioRecordErrorCode.E_NOSDCARD);
+            callBack.onWavAudioPrepareState(AudioRecordErrorCode.E_NOSDCARD);
+            return;
         }
     }
 
@@ -146,6 +148,7 @@ public class AudioRecorderManager {
             }
             beginTime = 0;
         }catch (Exception e){
+            LogUtils.YfcDebug("释放有异常："+e.getMessage());
             e.printStackTrace();
         }
     }
@@ -399,7 +402,6 @@ public class AudioRecorderManager {
      */
     public interface AudioDataCallBack {
         void onDataChange(int volume, float duration);
-        void onAudioPrepared(int state);
-        void onAudioPrepareError();
+        void onWavAudioPrepareState(int state);
     }
 }
