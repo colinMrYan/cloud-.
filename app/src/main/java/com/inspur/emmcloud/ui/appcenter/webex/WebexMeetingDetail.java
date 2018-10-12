d715e447676a3d9d0831c42a939ff8fc5467a626
chenmch <chenmch@inspur.com>
2018-10-12 10:54:20 +0800

    暂存

--- app/src/main/java/com/inspur/emmcloud/ui/appcenter/webex/WebexMeetingDetail.java
+++ app/src/main/java/com/inspur/emmcloud/ui/appcenter/webex/WebexMeetingDetail.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 
 import com.inspur.emmcloud.BaseActivity;
 import com.inspur.emmcloud.R;
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 
 import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
 
 /**
  * Created by chenmch on 2018/10/11.
@@ -13,6 +15,9 @@ import org.xutils.view.annotation.ContentView;
 
 @ContentView(R.layout.activity_webex_detail)
 public class WebexMeetingDetail extends BaseActivity {
+
+    @ViewInject(R.id.bt_function)
+    private QMUIRoundButton functionBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
