9f1aba526518df4ffe1f22c5a30d137363463d28
libaochao <libaochao@inspur.com>
2018-10-17 11:47:20 +0800

    华为手机沉浸问题解决1017

--- app/src/main/java/com/inspur/imp/api/ImpFragment.java
+++ app/src/main/java/com/inspur/imp/api/ImpFragment.java
@@ -41,6 +41,7 @@ import com.inspur.emmcloud.util.common.ResolutionUtils;
 import com.inspur.emmcloud.util.common.StringUtils;
 import com.inspur.emmcloud.util.privates.AppTabUtils;
 import com.inspur.emmcloud.util.privates.AppUtils;
+import com.inspur.emmcloud.util.privates.HonorImmersionStateBarUtils;
 import com.inspur.emmcloud.util.privates.ImageDisplayUtils;
 import com.inspur.emmcloud.util.privates.MDM.MDM;
 import com.inspur.emmcloud.util.privates.PreferencesByUsersUtils;
@@ -112,6 +113,7 @@ public class ImpFragment extends Fragment {
         rootView = inflater.inflate(Res.getLayoutID("activity_imp"), null);
         initViews();
         version = getArguments().getString(Constant.WEB_FRAGMENT_VERSION, "");
+        HonorImmersionStateBarUtils.setImmersiveStateBar(getActivity());
     }
 
     @Override
