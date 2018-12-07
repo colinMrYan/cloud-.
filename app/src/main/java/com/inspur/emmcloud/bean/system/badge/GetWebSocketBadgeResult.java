e9765ae36712c757f2e970f3b3f49e0e5effb4e2
Yu Fuchang <yufuchang@inspur.com>
2018-12-07 10:38:54 +0800

    增加对Action和Header的解析

--- app/src/main/java/com/inspur/emmcloud/bean/system/badge/GetWebSocketBadgeResult.java
+++ app/src/main/java/com/inspur/emmcloud/bean/system/badge/GetWebSocketBadgeResult.java
@@ -7,8 +7,12 @@ import com.inspur.emmcloud.util.common.JSONUtils;
  */
 
 public class GetWebSocketBadgeResult {
+    private BadgeActionModel badgeActionModel;
+    private BadgeHeadersModel badgeHeadersModel;
     private BadgeBodyModel badgeBodyModel;
     public GetWebSocketBadgeResult(String response){
+        badgeActionModel = new BadgeActionModel(JSONUtils.getString(response,"action",""));
+        badgeHeadersModel = new BadgeHeadersModel(JSONUtils.getString(response,"headers",""));
         badgeBodyModel = new BadgeBodyModel(JSONUtils.getString(response,"content",""));
     }
 
@@ -18,5 +22,21 @@ public class GetWebSocketBadgeResult {
 
     public void setBadgeBodyModel(BadgeBodyModel badgeBodyModel) {
         this.badgeBodyModel = badgeBodyModel;
+    }
+
+    public BadgeActionModel getBadgeActionModel() {
+        return badgeActionModel;
+    }
+
+    public void setBadgeActionModel(BadgeActionModel badgeActionModel) {
+        this.badgeActionModel = badgeActionModel;
+    }
+
+    public BadgeHeadersModel getBadgeHeadersModel() {
+        return badgeHeadersModel;
+    }
+
+    public void setBadgeHeadersModel(BadgeHeadersModel badgeHeadersModel) {
+        this.badgeHeadersModel = badgeHeadersModel;
     }
 }
