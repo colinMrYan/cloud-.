df6d10e2fc78b326c5523fa0c486dbc5f31af71d
libaochao <libaochao@inspur.com>
2018-12-20 11:44:17 +0800

    发件箱

---
+++ app/src/main/java/com/inspur/emmcloud/ui/appcenter/EmailSendActivity.java
@@ -0,0 +1,9 @@
+package com.inspur.emmcloud.ui.appcenter;
+
+import com.inspur.emmcloud.BaseActivity;
+
+/**
+ * Created by libaochao on 2018/12/20.
+ */
+public class EmailSendActivity extends BaseActivity {
+}
