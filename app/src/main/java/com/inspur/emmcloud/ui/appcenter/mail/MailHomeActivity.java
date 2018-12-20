35a771b39a3e2640e844beabb7f9328e128d49f8
chenmch <chenmch@inspur.com>
2018-12-20 16:55:22 +0800

    暂存

--- app/src/main/java/com/inspur/emmcloud/ui/appcenter/mail/MailHomeActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/appcenter/mail/MailHomeActivity.java
@@ -1,10 +1,20 @@
 package com.inspur.emmcloud.ui.appcenter.mail;
 
+import android.os.Bundle;
+
 import com.inspur.emmcloud.BaseActivity;
+import com.inspur.emmcloud.R;
+
+import org.xutils.view.annotation.ContentView;
 
 /**
  * Created by chenmch on 2018/12/20.
  */
 
+@ContentView(R.layout.activity_mail_home)
 public class MailHomeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
