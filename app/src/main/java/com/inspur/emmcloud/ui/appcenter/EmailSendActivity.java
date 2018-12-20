77baf99260bc67aef21041bc9d1222a15064d3c3
libaochao <libaochao@inspur.com>
2018-12-20 15:47:01 +0800

    邮件箱UI设计

--- app/src/main/java/com/inspur/emmcloud/ui/appcenter/EmailSendActivity.java
+++
@@ -1,9 +0,0 @@
-package com.inspur.emmcloud.ui.appcenter;
-
-import com.inspur.emmcloud.BaseActivity;
-
-/**
- * Created by libaochao on 2018/12/20.
- */
-public class EmailSendActivity extends BaseActivity {
-}
