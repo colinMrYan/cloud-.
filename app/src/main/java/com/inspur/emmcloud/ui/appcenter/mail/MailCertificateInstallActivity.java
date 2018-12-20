ae6b5d2e7c22a3a83595c6329a52c24fbc4a2aed
libaochao <libaochao@inspur.com>
2018-12-20 20:02:19 +0800

    邮件箱UI设计

---
+++ app/src/main/java/com/inspur/emmcloud/ui/appcenter/mail/MailCertificateInstallActivity.java
@@ -0,0 +1,10 @@
+package com.inspur.emmcloud.ui.appcenter.mail;
+
+import com.inspur.emmcloud.BaseActivity;
+
+/**
+ * Created by libaochao on 2018/12/20.
+ */
+
+public class MailCertificateInstallActivity extends BaseActivity {
+}
