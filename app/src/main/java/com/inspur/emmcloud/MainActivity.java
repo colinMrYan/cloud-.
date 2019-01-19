360b97d07d39410bfde8666c2506bcff5eddd605
Yu Fuchang <yufuchang@inspur.com>
2019-01-19 13:59:10 +0800

    群组信息页面编码

--- app/src/main/java/com/inspur/emmcloud/MainActivity.java
+++ app/src/main/java/com/inspur/emmcloud/MainActivity.java
@@ -21,7 +21,6 @@ import com.inspur.emmcloud.config.Constant;
 import com.inspur.emmcloud.config.MyAppConfig;
 import com.inspur.emmcloud.service.AppExceptionService;
 import com.inspur.emmcloud.ui.IndexActivity;
-import com.inspur.emmcloud.ui.contact.UserInfoActivity;
 import com.inspur.emmcloud.ui.login.LoginActivity;
 import com.inspur.emmcloud.ui.mine.setting.GuideActivity;
 import com.inspur.emmcloud.util.common.DensityUtil;
@@ -81,10 +80,10 @@ public class MainActivity extends BaseActivity { // 此处不能继承BaseActivi
             this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);//设置全屏
         }
         setContentView(R.layout.activity_main);
-//        checkNecessaryPermission();
-        Bundle bundle = new Bundle();
-        bundle.putString("uid","66666");
-        IntentUtils.startActivity(this, UserInfoActivity.class,bundle,true);
+        checkNecessaryPermission();
+//        Bundle bundle = new Bundle();
+//        bundle.putString(ConversationGroupInfoActivity.EXTRA_CID,"2");
+//        IntentUtils.startActivity(this, ConversationGroupInfoActivity.class,bundle,true);
     }
 
     private void checkNecessaryPermission() {
