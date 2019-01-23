12ae117c09ff93b36e2159525a51165700e0e366
chenmch <chenmch@inspur.com>
2019-01-23 10:12:29 +0800

    解决图片预览界面ui不协调的问题

--- app/src/main/java/com/inspur/imp/plugin/camera/imagepicker/ui/ImagePreviewActivity.java
+++ app/src/main/java/com/inspur/imp/plugin/camera/imagepicker/ui/ImagePreviewActivity.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 
 import com.inspur.emmcloud.R;
 import com.inspur.emmcloud.util.common.LogUtils;
-import com.inspur.emmcloud.util.common.StateBarUtils;
 import com.inspur.imp.plugin.camera.imageedit.IMGEditActivity;
 import com.inspur.imp.plugin.camera.imagepicker.ImagePicker;
 import com.inspur.imp.plugin.camera.imagepicker.bean.ImageItem;
@@ -193,14 +192,12 @@ public class ImagePreviewActivity extends ImagePreviewBaseActivity implements
 	/** 单击时，隐藏头和尾 */
 	@Override
 	public void onImageSingleTap() {
-		if (topBar.getVisibility() == View.VISIBLE) {
+		if (bottomBar.getVisibility() == View.VISIBLE) {
 			// topBar.setAnimation(AnimationUtils.loadAnimation(this,
 			// R.anim.top_out));
 			// bottomBar.setAnimation(AnimationUtils.loadAnimation(this,
 			// R.anim.fade_out));
-			topBar.setVisibility(View.GONE);
 			bottomBar.setVisibility(View.GONE);
-			StateBarUtils.translucent(this,R.color.transparent);
 			// tintManager.setStatusBarTintResource(R.color.transparent);//通知栏所需颜色
 			// 给最外层布局加上这个属性表示，Activity全屏显示，且状态栏被隐藏覆盖掉。
 			// if (Build.VERSION.SDK_INT >= 16)
@@ -210,9 +207,7 @@ public class ImagePreviewActivity extends ImagePreviewBaseActivity implements
 			// R.anim.top_in));
 			// bottomBar.setAnimation(AnimationUtils.loadAnimation(this,
 			// R.anim.fade_in));
-			topBar.setVisibility(View.VISIBLE);
 			bottomBar.setVisibility(View.VISIBLE);
-			StateBarUtils.translucent(this);
 			// tintManager.setStatusBarTintResource(R.color.status_bar);//通知栏所需颜色
 			// Activity全屏显示，但状态栏不会被隐藏覆盖，状态栏依然可见，Activity顶端布局部分会被状态遮住
 			// if (Build.VERSION.SDK_INT >= 16)
--- app/src/main/java/com/inspur/imp/plugin/camera/imagepicker/ui/ImagePreviewBaseActivity.java
+++ app/src/main/java/com/inspur/imp/plugin/camera/imagepicker/ui/ImagePreviewBaseActivity.java
@@ -1,17 +1,14 @@
 package com.inspur.imp.plugin.camera.imagepicker.ui;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.inspur.emmcloud.R;
 import com.inspur.imp.plugin.camera.imagepicker.ImagePicker;
 import com.inspur.imp.plugin.camera.imagepicker.adapter.ImagePageAdapter;
 import com.inspur.imp.plugin.camera.imagepicker.bean.ImageItem;
-import com.inspur.imp.plugin.camera.imagepicker.util.Utils;
 import com.inspur.imp.plugin.camera.imagepicker.view.ViewPagerFixed;
 
 import java.util.ArrayList;
@@ -41,14 +38,14 @@ public abstract class ImagePreviewBaseActivity extends ImageBaseActivity {
         isCanDelete = getIntent().getBooleanExtra("isCanDelete", true);
         //初始化控件
         content = findViewById(R.id.content);
-        if (Build.VERSION.SDK_INT >= 16) content.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        //因为状态栏透明后，布局整体会上移，所以给头部加上状态栏的margin值，保证头部不会被覆盖
+//        if (Build.VERSION.SDK_INT >= 16) content.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+//        //因为状态栏透明后，布局整体会上移，所以给头部加上状态栏的margin值，保证头部不会被覆盖
         topBar = findViewById(R.id.top_bar);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) topBar.getLayoutParams();
-            params.topMargin = Utils.getStatusHeight(this);
-            topBar.setLayoutParams(params);
-        }
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) topBar.getLayoutParams();
+//            params.topMargin = Utils.getStatusHeight(this);
+//            topBar.setLayoutParams(params);
+//        }
         topBar.findViewById(R.id.tv_ok).setVisibility(View.GONE);
         topBar.findViewById(R.id.btn_back).setOnClickListener(new View.OnClickListener() {
             @Override
