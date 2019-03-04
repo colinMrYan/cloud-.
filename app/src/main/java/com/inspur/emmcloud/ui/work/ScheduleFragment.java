ebd94a0a5b496fcbc4de6534f22e518d8eed293f
Yu Fuchang <yufuchang@inspur.com>
2019-03-04 13:23:35 +0800

    测试数据

--- app/src/main/java/com/inspur/emmcloud/ui/work/ScheduleFragment.java
+++ app/src/main/java/com/inspur/emmcloud/ui/work/ScheduleFragment.java
@@ -217,6 +217,7 @@ public class ScheduleFragment extends Fragment implements
                 calendarLayout.switchStatus();
             }
         });
+        initData();
     }
 
     private void initData() {
