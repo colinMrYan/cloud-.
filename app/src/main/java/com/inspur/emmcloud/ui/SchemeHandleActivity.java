a1635ca641378745a83c5c987fe1cdda64de5a15
Yu Fuchang <yufuchang@inspur.com>
2019-03-12 16:35:16 +0800

    权限逻辑修改，去掉统一电话权限分组，改为单个申请

--- app/src/main/java/com/inspur/emmcloud/ui/SchemeHandleActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/SchemeHandleActivity.java
@@ -124,7 +124,7 @@ public class SchemeHandleActivity extends Activity {
 
     private boolean isLackNecessaryPermission() {
         //如果没有存储权限则跳转到MainActivity进行处理
-        String[] necessaryPermissionArray = StringUtils.concatAll(Permissions.STORAGE, Permissions.PHONE_PERMISSION);
+        String[] necessaryPermissionArray = StringUtils.concatAll(Permissions.STORAGE, new String[]{Permissions.READ_PHONE_STATE});
         if (!PermissionRequestManagerUtils.getInstance().isHasPermission(MyApplication.getInstance(), necessaryPermissionArray)) {
             Intent intent = new Intent(SchemeHandleActivity.this, MainActivity.class);
             startActivity(intent);
