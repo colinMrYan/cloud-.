5212e9e2f584cb921a06448efc7ce6e664e4dd8f
libaochao <libaochao@inspur.com>
2019-03-27 15:46:09 +0800

    UI3.0 新日程编码

---
+++ app/src/main/java/com/inspur/emmcloud/ui/schedule/schedule/ScheduleSettingActivity.java
@@ -0,0 +1,10 @@
+package com.inspur.emmcloud.ui.schedule.schedule;
+
+import com.inspur.emmcloud.BaseActivity;
+
+/**
+ * Created by libaochao on 2019/3/27.
+ */
+
+public class ScheduleSettingActivity extends BaseActivity {
+}
