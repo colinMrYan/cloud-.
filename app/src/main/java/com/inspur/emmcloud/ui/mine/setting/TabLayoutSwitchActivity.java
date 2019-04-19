c00e5b17f52258aa0a1c92cc560c38ca4a04a3fc
Yu Fuchang <yufuchang@inspur.com>
2019-04-19 10:27:02 +0800

    切换布局增加确认提示dialog

--- app/src/main/java/com/inspur/emmcloud/ui/mine/setting/TabLayoutSwitchActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/mine/setting/TabLayoutSwitchActivity.java
@@ -21,6 +21,9 @@ import com.inspur.emmcloud.config.Constant;
 import com.inspur.emmcloud.ui.IndexActivity;
 import com.inspur.emmcloud.util.common.StringUtils;
 import com.inspur.emmcloud.util.privates.PreferencesByUserAndTanentUtils;
+import com.inspur.emmcloud.widget.dialogs.MyQMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
@@ -48,14 +51,8 @@ public class TabLayoutSwitchActivity extends BaseActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String currentTabLayoutName = PreferencesByUserAndTanentUtils.getString(MyApplication.getInstance(),Constant.APP_TAB_LAYOUT_NAME,"");
-                String selectedTabLayoutName = naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getName();
-                if(!currentTabLayoutName.equals(selectedTabLayoutName)){
-                    PreferencesByUserAndTanentUtils.putString(MyApplication.getInstance(),Constant.APP_TAB_LAYOUT_NAME,selectedTabLayoutName);
-                    Intent intent = new Intent(TabLayoutSwitchActivity.this, IndexActivity.class);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(intent);
-                }
+
+                showTabLayoutSwitch(i);
             }
         });
     }
@@ -83,25 +80,65 @@ public class TabLayoutSwitchActivity extends BaseActivity {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
             view = LayoutInflater.from(TabLayoutSwitchActivity.this).inflate(R.layout.mine_setting_tab_layout_list_item, null);
-            TextView themeNameText = view.findViewById(R.id.tv_tab_layout_name);
+            TextView tabLayoutNameText = view.findViewById(R.id.tv_tab_layout_name);
             ImageView selectImg = view.findViewById(R.id.iv_select);
-            Configuration config = getResources().getConfiguration();
-            String environmentLanguage = config.locale.getLanguage();
-            switch (environmentLanguage.toLowerCase()) {
-                case "zh-hant":
-                    themeNameText.setText(naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getNaviBarTitleResult().getZhHans());
-                    break;
-                case "en":
-                case "en-us":
-                    themeNameText.setText(naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getNaviBarTitleResult().getEnUS());
-                    break;
-                default:
-                    themeNameText.setText(naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getNaviBarTitleResult().getZhHans());
-                    break;
-            }
+            tabLayoutNameText.setText(getTabLayoutName(i));
             selectImg.setVisibility(getSelectedShow(i));
             return view;
         }
+    }
+
+    /**
+     * 切换布局国际化语言
+     * @param i
+     * @return
+     */
+    private String getTabLayoutName(int i) {
+        String tabLayoutName = "";
+        Configuration config = getResources().getConfiguration();
+        String environmentLanguage = config.locale.getLanguage();
+        switch (environmentLanguage.toLowerCase()) {
+            case "zh-hant":
+                tabLayoutName = naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getNaviBarTitleResult().getZhHans();
+                break;
+            case "en":
+            case "en-us":
+                tabLayoutName = naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getNaviBarTitleResult().getEnUS();
+                break;
+            default:
+                tabLayoutName = naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(i).getNaviBarTitleResult().getZhHans();
+                break;
+        }
+        return tabLayoutName;
+    }
+
+    /**
+     * 确认切换布局的dialog
+     * @param selectIndex
+     */
+    private void showTabLayoutSwitch(final int selectIndex){
+        final String currentTabLayoutName = PreferencesByUserAndTanentUtils.getString(MyApplication.getInstance(),Constant.APP_TAB_LAYOUT_NAME,"");
+        final String selectedTabLayoutName = naviBarModel.getNaviBarPayload().getNaviBarSchemeList().get(selectIndex).getName();
+        new MyQMUIDialog.MessageDialogBuilder(this)
+                .setMessage(getString(R.string.mine_tab_layout_switch,getTabLayoutName(selectIndex)))
+                .addAction(R.string.cancel, new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                    }
+                })
+                .addAction(R.string.ok, new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        if(!currentTabLayoutName.equals(selectedTabLayoutName)){
+                            PreferencesByUserAndTanentUtils.putString(MyApplication.getInstance(),Constant.APP_TAB_LAYOUT_NAME,selectedTabLayoutName);
+                            Intent intent = new Intent(TabLayoutSwitchActivity.this, IndexActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        }
+                    }
+                })
+                .show();
     }
 
     /**
