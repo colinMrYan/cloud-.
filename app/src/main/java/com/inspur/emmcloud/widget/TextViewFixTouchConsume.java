351f30056d5108342c576e64cfb4f599008ef8b4
libaochao <libaochao@inspur.com>
2019-05-01 18:05:57 +0800

    点击事件

--- app/src/main/java/com/inspur/emmcloud/widget/TextViewFixTouchConsume.java
+++ app/src/main/java/com/inspur/emmcloud/widget/TextViewFixTouchConsume.java
@@ -11,6 +11,8 @@ import android.text.method.Touch;
 import android.text.style.ClickableSpan;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.TextView;
 
 import com.inspur.emmcloud.util.common.LogUtils;
@@ -22,7 +24,6 @@ import com.inspur.emmcloud.util.common.LogUtils;
 public class TextViewFixTouchConsume extends TextView {
     boolean dontConsumeNonUrlClicks = true;
     boolean linkHit;
-
 
     public TextViewFixTouchConsume(Context context) {
         super(context);
@@ -40,9 +41,11 @@ public class TextViewFixTouchConsume extends TextView {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         linkHit = false;
-         super.onTouchEvent(event);
-            return linkHit;
+        boolean res = super.onTouchEvent(event);
 
+        if (dontConsumeNonUrlClicks)
+            return linkHit;
+        return res;
 
     }
 
@@ -56,6 +59,9 @@ public class TextViewFixTouchConsume extends TextView {
     public static class LocalLinkMovementMethod extends LinkMovementMethod {
         static LocalLinkMovementMethod sInstance;
         private long lastClickTime;
+        private static final long CLICK_DELAY = 500l;
+
+
 
         public static LocalLinkMovementMethod getInstance() {
             if (sInstance == null)
@@ -68,9 +74,6 @@ public class TextViewFixTouchConsume extends TextView {
         public boolean onTouchEvent(TextView widget,
                                     Spannable buffer, MotionEvent event) {
             int action = event.getAction();
-            if (action == MotionEvent.ACTION_DOWN){
-                lastClickTime = System.currentTimeMillis();
-            }
 
             if (action == MotionEvent.ACTION_UP ||
                     action == MotionEvent.ACTION_DOWN) {
@@ -91,20 +94,31 @@ public class TextViewFixTouchConsume extends TextView {
                         off, off, ClickableSpan.class);
 
                 if (link.length != 0) {
-                    if (System.currentTimeMillis() - lastClickTime < 3000) {
-                        if (action == MotionEvent.ACTION_UP) {
-                            link[0].onClick(widget);
-                            if (widget instanceof TextViewFixTouchConsume) {
-                                ((TextViewFixTouchConsume) widget).linkHit = true;
+                    if (action == MotionEvent.ACTION_UP) {
+                        if (System.currentTimeMillis() - lastClickTime > CLICK_DELAY) {
+                            ViewParent parent = widget.getParent();
+                            if (parent instanceof ViewGroup) {
+                                // 获取被点击控件的父容器，让父容器执行点击；
+                                ((ViewGroup) parent).performClick();
                             }
-                            LogUtils.jasonDebug("0000000000000000000000");
-                        } else if (action == MotionEvent.ACTION_DOWN) {
-                            LogUtils.jasonDebug("1111111111111111111111111");
+                            LogUtils.LbcDebug("22222222222222222222222222");
+                            return true;
+                        }else{
+                            //点击事件
+                            link[0].onClick(widget);
+                            LogUtils.LbcDebug("11111111111111111111111111");
+                            return true;
                         }
+                       // link[0].onClick(widget);
+                    } else if (action == MotionEvent.ACTION_DOWN) {
+                        Selection.setSelection(buffer,
+                                buffer.getSpanStart(link[0]),
+                                buffer.getSpanEnd(link[0]));
                     }
 
-
-
+                    if (widget instanceof TextViewFixTouchConsume) {
+                        ((TextViewFixTouchConsume) widget).linkHit = true;
+                    }
                     return true;
                 } else {
                     Selection.removeSelection(buffer);
