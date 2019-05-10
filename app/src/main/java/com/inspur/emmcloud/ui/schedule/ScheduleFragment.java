2de6020167d7e8076d14d7d5db5d9ce0431945ad
Chen Mingchao <chenmch@inspur.com>
2019-05-10 17:22:55 +0800

    日视图线程处理

--- app/src/main/java/com/inspur/emmcloud/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/inspur/emmcloud/ui/schedule/ScheduleFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
