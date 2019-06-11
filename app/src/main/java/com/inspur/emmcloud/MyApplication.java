fc7dc47398efd9fac46d02209da58d1266bc24b8
Chen Mingchao <chenmch@inspur.com>
2019-06-11 11:33:46 +0800

    去掉RN

--- app/src/main/java/com/inspur/emmcloud/MyApplication.java
+++ app/src/main/java/com/inspur/emmcloud/MyApplication.java
@@ -1,47 +1,16 @@
 package com.inspur.emmcloud;
 
-import com.beefe.picker.PickerViewPackage;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.horcrux.svg.SvgPackage;
 import com.inspur.emmcloud.basemodule.application.BaseApplication;
 import com.inspur.emmcloud.login.communication.CommunicationService;
-import com.inspur.reactnative.AuthorizationManagerPackage;
 import com.luojilab.component.componentlib.router.Router;
 import com.luojilab.component.componentlib.router.ui.UIRouter;
-import com.oblador.vectoricons.VectorIconsPackage;
-
-import java.util.Arrays;
-import java.util.List;
 
 
 /**
  * Application class
  */
-public class MyApplication extends BaseApplication implements ReactApplication {
-    /**
-     * ReactNative相关代码
-     */
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return com.facebook.react.BuildConfig.DEBUG;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.asList(
-                    new MainReactPackage(),
-                    new AuthorizationManagerPackage(),
-                    new PickerViewPackage(),
-                    new SvgPackage(),
-                    new VectorIconsPackage()
-            );
-        }
-    };
+public class MyApplication extends BaseApplication {
 
     public void onCreate() {
         super.onCreate();
@@ -56,13 +25,4 @@ public class MyApplication extends BaseApplication implements ReactApplication {
         SoLoader.init(this, false);//ReactNative相关初始化
     }
 
-    /**
-     * ReactNative相关代码
-     *
-     * @return
-     */
-    @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
-    }
 }
