a816adfb3b0ab12b93e34aa482ab10d109cd90be
libaochao <libaochao@inspur.com>
2019-06-28 16:00:19 +0800

    弹出虚拟键

--- app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationActivity.java
@@ -838,6 +838,7 @@ public class ConversationActivity extends ConversationBaseActivity {
                         if (!originalPicture) {
                             imgPath = previewImgPath;
                         }
+                        //  DisplayMediaImageMsg.getImgViewSize(this,)
                         combinAndSendMessageWithFile(imgPath, Message.MESSAGE_TYPE_MEDIA_IMAGE, previewImgPath);
                     }
                 }
