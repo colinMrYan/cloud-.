3eb68e1d1a54f96e72351683d274414d37151ba2
Chen Mingchao <chenmch@inspur.com>
2019-07-19 13:57:34 +0800

    解决模块单独运行时不能跳转到登录的问题

--- basemodule/src/main/java/com/inspur/emmcloud/basemodule/application/BaseApplication.java
+++ basemodule/src/main/java/com/inspur/emmcloud/basemodule/application/BaseApplication.java
@@ -91,6 +91,10 @@ public abstract class BaseApplication extends MultiDexApplication {
     private void init() {
         // TODO Auto-generated method stub
         instance = this;
+        Router.registerComponent("com.inspur.emmcloud.applike.AppApplike");
+        Router.registerComponent("com.inspur.emmcloud.login.applike.LoginAppLike");
+        Router.registerComponent("com.inspur.emmcloud.web.applike.WebAppLike");
+        Router.registerComponent("com.inspur.emmcloud.news.applike.NewsAppLike");
         CrashHandler crashHandler = CrashHandler.getInstance();
         crashHandler.init(getInstance());
         x.Ext.init(BaseApplication.this);
