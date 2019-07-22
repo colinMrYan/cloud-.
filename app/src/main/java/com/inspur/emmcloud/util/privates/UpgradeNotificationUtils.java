164566a2595068d7c98682f03cf34d5d27d8af30
libaochao <libaochao@inspur.com>
2019-07-22 14:06:54 +0800

    更新优化

--- app/src/main/java/com/inspur/emmcloud/util/privates/UpgradeNotificationUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/privates/UpgradeNotificationUtils.java
@@ -1,5 +1,6 @@
 package com.inspur.emmcloud.util.privates;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
