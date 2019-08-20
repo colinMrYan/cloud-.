8d21654b4014ba45af4428d5bb475711dd9b22ef
libaochao <libaochao@inspur.com>
2019-08-20 13:55:20 +0800

    沟通

---
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/CommunicationSearchModelMoreActivity.java
@@ -0,0 +1,26 @@
+package com.inspur.emmcloud.ui.chat;
+
+import com.inspur.emmcloud.R;
+import com.inspur.emmcloud.basemodule.ui.BaseActivity;
+
+/**
+ * Created by libaochao on 2019/8/20.
+ */
+
+public class CommunicationSearchModelMoreActivity extends BaseActivity {
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public int getLayoutResId() {
+        return R.layout.communication_search_model_detail_activity;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
