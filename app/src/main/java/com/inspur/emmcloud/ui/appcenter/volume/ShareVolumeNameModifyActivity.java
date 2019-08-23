a1102a8df18282217fac4b12c021040d30786584
Yu Fuchang <yufuchang@inspur.com>
2019-08-23 09:52:03 +0800

    名称显示问题

--- app/src/main/java/com/inspur/emmcloud/ui/appcenter/volume/ShareVolumeNameModifyActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/appcenter/volume/ShareVolumeNameModifyActivity.java
@@ -34,6 +34,8 @@ public class ShareVolumeNameModifyActivity extends BaseActivity {
 
     @BindView(R.id.header_text)
     TextView headerText;
+    @BindView(R.id.tv_edit_gropName_tip)
+    TextView groupTitleText;
 
     private LoadingDialog loadingDlg;
     private Volume volume;
@@ -54,6 +56,7 @@ public class ShareVolumeNameModifyActivity extends BaseActivity {
         EditTextUtils.setText(editText, isVolumeNameModify ? volume.getName() : group.getName());
         editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(MyAppConfig.VOLUME_MAX_FILE_NAME_LENGTH)});
         loadingDlg = new LoadingDialog(ShareVolumeNameModifyActivity.this);
+        groupTitleText.setText(R.string.clouddriver_volume_group_name);
     }
 
     @Override
