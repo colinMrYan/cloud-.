ff96c4af8dfb49aec2c22aa5314336d559efaff6
zhangyj <zhangyj.lc@inspur.com>
2019-09-02 10:11:40 +0800

    微信电子发票调研功能提交

---
+++ basemodule/src/main/java/com/inspur/emmcloud/basemodule/bean/WechatCardBean.java
@@ -0,0 +1,9 @@
+package com.inspur.emmcloud.basemodule.bean;
+
+import java.io.Serializable;
+
+public class WechatCardBean implements Serializable {
+    public String card_id;
+    public String encrypt_code;
+    public String app_id;
+}
