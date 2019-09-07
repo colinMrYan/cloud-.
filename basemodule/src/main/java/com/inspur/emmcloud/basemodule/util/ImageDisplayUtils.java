c658badf76c522a73a65408b1a193ad5ba4c00ba
Chen Mingchao <chenmch@inspur.com>
2019-09-07 11:48:38 +0800

    解决图片显示bug

--- basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/ImageDisplayUtils.java
+++ basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/ImageDisplayUtils.java
@@ -114,14 +114,14 @@ public class ImageDisplayUtils {
                 // 设置图片的解码类型
                 .bitmapConfig(Bitmap.Config.RGB_565)
                 .cacheInMemory(true)
-                .cacheOnDisk(!uri.startsWith("drawable:"))
+                .cacheOnDisk(!(uri != null && uri.startsWith("drawable:")))
                 .considerExifParams(true)
                 .build();
         finalShowImg(uri, imageView, options);
     }
 
     private void finalShowImg(String uri, ImageView imageView, DisplayImageOptions options) {
-        if (uri.startsWith("drawable://")) {
+        if (uri != null && uri.startsWith("drawable://")) {
             //此处防止drawable太大导致内存溢出
             try {
                 String[] drawableIds = uri.split("drawable://");
@@ -259,7 +259,7 @@ public class ImageDisplayUtils {
                 .showImageOnLoading(defaultDrawableId)
                 .considerExifParams(true)
                 .cacheInMemory(true)
-                .cacheOnDisk(StringUtils.isBlank(uri) || !uri.startsWith("drawable:"))
+                .cacheOnDisk(!(uri != null && uri.startsWith("drawable:")))
                 .build();
         return options;
     }
