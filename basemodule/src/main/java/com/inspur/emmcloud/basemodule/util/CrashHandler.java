4af83cafc0dfbb45641e08c42bd183f71883d4c9
Yu Fuchang <yufuchang@inspur.com>
2019-09-11 14:42:14 +0800

    完善逻辑封装方法

--- basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/CrashHandler.java
+++ basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/CrashHandler.java
@@ -1,20 +1,15 @@
 package com.inspur.emmcloud.basemodule.util;
 
 import android.content.Context;
-import android.os.Build;
-import android.os.StrictMode;
 import android.util.Log;
 
 import com.inspur.emmcloud.baselib.util.PreferencesUtils;
-import com.inspur.emmcloud.basemodule.api.BaseModuleApiUri;
+import com.inspur.emmcloud.basemodule.api.BaseModuleApiService;
 import com.inspur.emmcloud.basemodule.application.BaseApplication;
 import com.inspur.emmcloud.basemodule.bean.AppException;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.xutils.http.HttpMethod;
-import org.xutils.http.RequestParams;
-import org.xutils.x;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -45,16 +40,22 @@ public class CrashHandler implements UncaughtExceptionHandler {
         return mInstance;
     }
 
+    /**
+     * 发生异常后先保存异常，然后当场上传，上传完成返回成功根据时间戳和内容删除异常记录
+     *
+     * @param thread
+     * @param throwable
+     */
     @Override
     public void uncaughtException(Thread thread, Throwable throwable) {
         // 把错误的堆栈信息 获取出来
         String errorInfo = getErrorInfo(throwable);
         Log.d("jason", "errorInfo=" + errorInfo);
         Log.e("AndroidRuntime", errorInfo);
-        AppExceptionCacheUtils.saveAppException(mContext, 1, "", errorInfo, 0);
         AppException appException = new AppException(System.currentTimeMillis(), AppUtils.getVersion(mContext), 1, "", errorInfo, 0);
+        AppExceptionCacheUtils.saveAppException(mContext, appException);
 //        AppException appException = AppExceptionCacheUtils.getAppExceptionListByLevel(mContext, 1);
-        uploadException(mContext, appException);
+        new BaseModuleApiService(mContext).uploadException(mContext, appException, getUploadContentJSONObj(appException));
         //如果系统提供了默认的异常处理器，则交给系统去结束我们的程序，否则就由我们自己结束自己
         if (mDefaultHandler != null) {
             mDefaultHandler.uncaughtException(thread, throwable);
@@ -68,37 +69,6 @@ public class CrashHandler implements UncaughtExceptionHandler {
             // 干掉当前的程序
             android.os.Process.killProcess(android.os.Process.myPid());
             System.exit(1);
-        }
-    }
-
-    /**
-     * 上传异常,修改严格模式设置使异常上传放在主线程执行
-     * @param mContext
-     */
-    private void uploadException(final Context mContext, final AppException appException) {
-        JSONObject jsonObject = getUploadContentJSONObj(appException);
-        if (NetUtils.isNetworkConnected(mContext, false) && !AppUtils.isApkDebugable(mContext)) {
-            final String completeUrl = BaseModuleApiUri.getUploadExceptionUrl();
-            RequestParams params = ((BaseApplication) mContext.getApplicationContext()).getHttpRequestParams(completeUrl);
-            params.setAsJsonContent(true);
-            params.setBodyContent(jsonObject.toString());
-
-            //由于Android3.0之后已经不能在主线程发起网络请求，因为会造成ANR，但此处情况特殊，需临时关闭StrictMode
-            StrictMode.ThreadPolicy oldThreadPolicy = StrictMode.getThreadPolicy();
-            StrictMode.VmPolicy oldVmPolicy = StrictMode.getVmPolicy();
-            if (Build.VERSION.SDK_INT >= 11) {
-                StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
-                StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
-            }
-            try {
-                x.http().requestSync(HttpMethod.POST, params, JSONObject.class);
-                AppExceptionCacheUtils.deleteAppExceptionByContentAndHappenTime(mContext, appException.getHappenTime(), appException.getErrorInfo());
-            } catch (Throwable throwable) {
-                throwable.printStackTrace();
-            }
-            //时候后改回StrictMode
-            StrictMode.setThreadPolicy(oldThreadPolicy);
-            StrictMode.setVmPolicy(oldVmPolicy);
         }
     }
 
@@ -143,11 +113,6 @@ public class CrashHandler implements UncaughtExceptionHandler {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return contentObj;
     }
-
-
-
-
 }
