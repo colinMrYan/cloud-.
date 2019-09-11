10c001000836686094ae7cfe033c6fcf88ea0a86
libaochao <libaochao@inspur.com>
2019-09-11 12:20:53 +0800

    @All 所有人

--- app/src/main/java/com/inspur/emmcloud/ui/chat/MembersActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/MembersActivity.java
@@ -1,13 +1,17 @@
 package com.inspur.emmcloud.ui.chat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -23,7 +27,9 @@ import com.inspur.emmcloud.baselib.util.DensityUtil;
 import com.inspur.emmcloud.baselib.util.PinyinUtils;
 import com.inspur.emmcloud.baselib.util.StringUtils;
 import com.inspur.emmcloud.baselib.util.ToastUtils;
+import com.inspur.emmcloud.baselib.widget.FlowLayout;
 import com.inspur.emmcloud.baselib.widget.LoadingDialog;
+import com.inspur.emmcloud.baselib.widget.MaxHightScrollView;
 import com.inspur.emmcloud.basemodule.config.Constant;
 import com.inspur.emmcloud.basemodule.ui.BaseActivity;
 import com.inspur.emmcloud.basemodule.util.WebServiceRouterManager;
@@ -40,6 +46,7 @@ import com.inspur.emmcloud.widget.ECMSpaceItemDecoration;
 import com.inspur.emmcloud.widget.slidebar.CharacterParser;
 import com.inspur.emmcloud.widget.slidebar.SideBar;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.Serializable;
@@ -73,6 +80,14 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
     TextView userHeadText;
     @BindView(R.id.ev_channel_member_search_input)
     EditText searchInputEv;
+    @BindView(R.id.tv_more_select)
+    TextView moreSelectText;
+    @BindView(R.id.flow_layout)
+    FlowLayout flowLayout;
+    @BindView(R.id.search_edit_layout)
+    MaxHightScrollView searchEditLayout;
+
+    EditText editText;
     private CharacterParser characterParser;// 汉字转拼音
     private PinyinComparator pinyinComparator;// 根据拼音来排列ListView里面的数据类
     private ChannelMemberListAdapter channelMemberListAdapter;
@@ -84,7 +99,7 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
     private MemberSelectGridAdapter selectGridAdapter;
     private List<ContactUser> allReadySelectUserList = new ArrayList<>();
     private List<PersonDto> newSelectUserList = new ArrayList<>();//这次刚选的群成员list
-    private List<PersonDto> selectedUserList = new ArrayList<>();//选中的群成员list
+    private List<PersonDto> selectedUserList = new ArrayList<>();//选中的群成员list  ///
     private List<PersonDto> allReadySelectPersonDtoList = new ArrayList<>();
     private int state = -1;
 
@@ -103,8 +118,13 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
 
     private void initViews() {
         state = getIntent().getIntExtra(MEMBER_PAGE_STATE, -1);
-        if (state == MENTIONS_STATE) {
-            findViewById(R.id.rl_select_all).setVisibility(View.VISIBLE);
+        if (state == MENTIONS_STATE) {  ///////////////////////////
+            lettersSideBar.setVisibility(View.GONE);
+            moreSelectText.setVisibility(View.VISIBLE);
+            okTv.setVisibility(View.GONE);
+            flowAddEdit();
+        } else {
+            selectedUserList.add(tranContactUser2PersonDto(ContactUserCacheUtils.getContactUserByUid(MyApplication.getInstance().getUid())));
         }
         loadingDlg = new LoadingDialog(this);
         loadingDlg.show();
@@ -113,14 +133,13 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
         allReadySelectUserList = new ArrayList<>();
         allReadySelectUserList.add(ContactUserCacheUtils.getContactUserByUid(MyApplication.getInstance().getUid()));
         allReadySelectPersonDtoList = tranContactUserList2PersonDtoList(allReadySelectUserList);
-        selectedUserList.add(tranContactUser2PersonDto(ContactUserCacheUtils.getContactUserByUid(MyApplication.getInstance().getUid())));
         selectGridAdapter = new MemberSelectGridAdapter(this, allReadySelectPersonDtoList);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 5);
         selectedMemberRecylerView.addItemDecoration(new ECMSpaceItemDecoration(DensityUtil.dip2px(this, 8)));
         selectedMemberRecylerView.setLayoutManager(gridLayoutManager);
         selectedMemberRecylerView.setAdapter(selectGridAdapter);
         selectedMemberRecylerView.setVisibility((state == SELECT_STATE && allReadySelectUserList.size() > 0) ? View.VISIBLE : View.GONE);
-        lettersSideBar.setVisibility(state == SELECT_STATE ? View.GONE : View.VISIBLE);
+        lettersSideBar.setVisibility(state == SELECT_STATE || state == MENTIONS_STATE ? View.GONE : View.VISIBLE);
         channelId = getIntent().getStringExtra("cid");
         userHeadText.setText(getIntent().getStringExtra("title"));
     }
@@ -145,6 +164,14 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
                     personDtoList = ContactUserCacheUtils.getShowMemberList(getIntent().getStringArrayListExtra("uidList"));
                 }
                 if (state == MENTIONS_STATE) {
+                    PersonDto personDtoAdd = new PersonDto();
+                    personDtoAdd.setName(getString(R.string.chat_search_mention_all));
+                    personDtoAdd.setUid("10");
+                    personDtoAdd.setPinyinFull("#quanbuchengyuan");
+                    personDtoAdd.setSuoxie("#quanbuchengyuan");
+                    personDtoAdd.setUtype("");
+                    personDtoAdd.setSortLetters("#");
+                    personDtoList.add(0, personDtoAdd);
                     Iterator<PersonDto> personDtoIterator = personDtoList.iterator();
                     while (personDtoIterator.hasNext()) {
                         PersonDto personDto = personDtoIterator.next();
@@ -200,6 +227,74 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
     }
 
     /**
+     * 刷新FlowLayout
+     */
+    private void notifyFlowLayoutDataChange() {
+        editText.setText("");
+        flowLayout.removeAllViews();
+        for (int i = 0; i < selectedUserList.size(); i++) {
+            final PersonDto personDto = selectedUserList.get(i);
+            TextView searchResultText = new TextView(this);
+            FlowLayout.LayoutParams params = new FlowLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.leftMargin = DensityUtil.dip2px(MyApplication.getInstance(), 5);
+            params.topMargin = DensityUtil.dip2px(MyApplication.getInstance(), 2);
+            params.bottomMargin = params.topMargin;
+            searchResultText.setLayoutParams(params);
+            int piddingTop = DensityUtil.dip2px(this.getApplicationContext(), 1);
+            int piddingLeft = DensityUtil.dip2px(this.getApplicationContext(), 5);
+            searchResultText.setPadding(piddingLeft, piddingTop, piddingLeft, piddingTop);
+            searchResultText.setGravity(Gravity.CENTER);
+            searchResultText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+            searchResultText.setTextColor(Color.parseColor("#0F7BCA"));
+            searchResultText.setText(selectedUserList.get(i).getName());
+            searchResultText.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    changePersonDtos(personDto);
+                }
+            });
+            flowLayout.addView(searchResultText);
+        }
+        flowAddEdit();
+    }
+
+    private void changePersonDtos(PersonDto personDto) {
+        if (selectedUserList.contains(personDto)) {
+            selectedUserList.remove(personDto);
+            channelMemberListAdapter.updateSelectListViewData(selectedUserList);
+            selectGridAdapter.setAndRefreshSelectMemberData(selectedUserList);
+            notifyFlowLayoutDataChange();
+        }
+    }
+
+    private void flowAddEdit() {
+        if (editText == null) {
+            editText = new EditText(this);
+            FlowLayout.LayoutParams params = new FlowLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT, DensityUtil.dip2px(
+                    this.getApplicationContext(), ViewGroup.LayoutParams.WRAP_CONTENT));
+            params.topMargin = DensityUtil.dip2px(this.getApplicationContext(), 2);
+            params.bottomMargin = params.topMargin;
+            int piddingTop = DensityUtil.dip2px(MyApplication.getInstance(), 1);
+            int piddingLeft = DensityUtil.dip2px(MyApplication.getInstance(), 10);
+            editText.setPadding(piddingLeft, piddingTop, piddingLeft, piddingTop);
+            editText.setLayoutParams(params);
+            editText.setSingleLine(true);
+            editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+            editText.setBackground(null);
+            editText.setHint(getString(R.string.msg_key_search_member));
+            editText.addTextChangedListener(this);
+        }
+
+        if (editText.getParent() == null) {
+            flowLayout.addView(editText);
+        }
+    }
+
+    /**
      * 备用方法，获取索引letters
      *
      * @return
@@ -247,21 +342,39 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
                         }
                         break;
                     case MENTIONS_STATE:
-                        JSONObject jsonResult = new JSONObject();
-                        try {
-                            String uid, name;
-                            uid = (filterList.size() != 0) ? filterList.get(position).getUid() : personDtoList.get(position).getUid();
-                            name = (filterList.size() != 0) ? filterList.get(position).getName() : personDtoList.get(position).getName();
-                            jsonResult.put("uid", uid);
-                            jsonResult.put("name", name);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        PersonDto MentionDto = (filterList.size() != 0) ? filterList.get(position) : personDtoList.get(position);
+                        if (moreSelectText.getVisibility() == View.VISIBLE || MentionDto.getUid().equals("10")) {
+                            JSONArray jsonArray = new JSONArray();
+                            try {
+                                JSONObject jsonObject = new JSONObject();
+                                jsonObject.put("uid", MentionDto.getUid());
+                                jsonObject.put("name", MentionDto.getName());
+                                jsonArray.put(jsonObject);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            intent.putExtra("searchResult", jsonArray.toString());
+                            boolean isInputKeyWord = getIntent().getBooleanExtra("isInputKeyWord", false);
+                            intent.putExtra("isInputKeyWord", isInputKeyWord);
+                            setResult(RESULT_OK, intent);
+                            finish();
+                        } else {
+                            if (!allReadySelectPersonDtoList.contains(MentionDto)) {
+                                if (!selectedUserList.contains(MentionDto)) {
+                                    selectedUserList.add(MentionDto);
+                                    newSelectUserList.add(MentionDto);
+                                    updateView(view, View.VISIBLE);
+                                } else {
+                                    selectedUserList.remove(MentionDto);
+                                    newSelectUserList.remove(MentionDto);
+                                    updateView(view, View.GONE);
+                                }
+                                channelMemberListAdapter.updateSelectListViewData(selectedUserList);
+                                selectGridAdapter.setAndRefreshSelectMemberData(selectedUserList);
+                                allMemberListView.setSelection(position);
+                            }
+                            notifyFlowLayoutDataChange();
                         }
-                        intent.putExtra("searchResult", jsonResult.toString());
-                        boolean isInputKeyWord = getIntent().getBooleanExtra("isInputKeyWord", false);
-                        intent.putExtra("isInputKeyWord", isInputKeyWord);
-                        setResult(RESULT_OK, intent);
-                        finish();
                         break;
                     case CHECK_STATE:
                         String uid = "";
@@ -308,7 +421,7 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
      * @param visible
      */
     private void updateView(View view, int visible) {
-        ImageView imageView = (ImageView) view.findViewById(R.id.img_member_selected);
+        ImageView imageView = view.findViewById(R.id.img_member_selected);
         imageView.setVisibility(visible);
         imageView.setImageResource(R.drawable.icon_other_selected);
     }
@@ -402,7 +515,9 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
         // 根据a-z进行排序
         Collections.sort(filterDateList, pinyinComparator);
         channelMemberListAdapter.updateListView(filterDateList);
-        allMemberListView.setSelection(0);
+        if (state != MENTIONS_STATE) {
+            allMemberListView.setSelection(0);
+        }
     }
 
     /**
@@ -434,22 +549,33 @@ public class MembersActivity extends BaseActivity implements TextWatcher {
                 finish();
                 break;
             case R.id.tv_ok:
-                startCommunication();
-                break;
-            case R.id.rl_select_all:
-                JSONObject jsonResult = new JSONObject();
-                Intent intent = new Intent();
-                try {
-                    jsonResult.put("uid", 10);
-                    jsonResult.put("name", getString(R.string.chat_search_mention_all));
-                } catch (Exception e) {
-                    e.printStackTrace();
+                if (state == MENTIONS_STATE) {
+                    Intent intent = new Intent();
+                    JSONArray jsonArray = new JSONArray();
+                    for (int i = 0; i < selectedUserList.size(); i++) {
+                        JSONObject jsonResult1 = new JSONObject();
+                        try {
+                            jsonResult1.put("uid", selectedUserList.get(i).getUid());
+                            jsonResult1.put("name", selectedUserList.get(i).getName());
+                            jsonArray.put(jsonResult1);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    intent.putExtra("searchResult", jsonArray.toString());
+                    boolean isInputKeyWord = getIntent().getBooleanExtra("isInputKeyWord", false);
+                    intent.putExtra("isInputKeyWord", isInputKeyWord);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                    finish();
+                } else {
+                    startCommunication();
                 }
-                intent.putExtra("searchResult", jsonResult.toString());
-                boolean isInputKeyWord = getIntent().getBooleanExtra("isInputKeyWord", false);
-                intent.putExtra("isInputKeyWord", isInputKeyWord);
-                setResult(RESULT_OK, intent);
-                finish();
+
+                break;
+            case R.id.tv_more_select:
+                okTv.setVisibility(View.VISIBLE);
+                moreSelectText.setVisibility(View.GONE);
                 break;
             default:
                 break;
