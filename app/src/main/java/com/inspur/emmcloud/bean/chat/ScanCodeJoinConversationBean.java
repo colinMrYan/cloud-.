5437ee4575a48c0b4e5789d2edca349fef6042cf
Yu Fuchang <yufuchang@inspur.com>
2019-09-13 14:02:49 +0800

    增加实体解析类

---
+++ app/src/main/java/com/inspur/emmcloud/bean/chat/ScanCodeJoinConversationBean.java
@@ -0,0 +1,23 @@
+package com.inspur.emmcloud.bean.chat;
+
+import com.inspur.emmcloud.baselib.util.JSONUtils;
+
+/**
+ * Created by: yufuchang
+ * Date: 2019/9/13
+ */
+public class ScanCodeJoinConversationBean {
+    String conversationQrCode = "";
+
+    public ScanCodeJoinConversationBean(String response) {
+        conversationQrCode = JSONUtils.getString(response, "", "");
+    }
+
+    public String getConversationQrCode() {
+        return conversationQrCode;
+    }
+
+    public void setConversationQrCode(String conversationQrCode) {
+        this.conversationQrCode = conversationQrCode;
+    }
+}
