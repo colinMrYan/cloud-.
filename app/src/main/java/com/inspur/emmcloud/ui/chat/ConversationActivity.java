e627064c0c76e6f2d4cee7703d792f233205492f
libaochao <libaochao@inspur.com>
2019-10-29 13:52:48 +0800

    搜索添加撤回消息限制

--- app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationActivity.java
@@ -30,6 +30,7 @@ import com.inspur.emmcloud.api.apiservice.ChatAPIService;
 import com.inspur.emmcloud.api.apiservice.WSAPIService;
 import com.inspur.emmcloud.baselib.util.IntentUtils;
 import com.inspur.emmcloud.baselib.util.JSONUtils;
+import com.inspur.emmcloud.baselib.util.LogUtils;
 import com.inspur.emmcloud.baselib.util.StringUtils;
 import com.inspur.emmcloud.baselib.util.ToastUtils;
 import com.inspur.emmcloud.baselib.widget.CustomLoadingView;
