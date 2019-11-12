2f83e61a5d55e4b5d1d8460764d9d196ff4a080d
libaochao <libaochao@inspur.com>
2019-11-12 15:41:04 +0800

    网盘优化

--- app/src/main/java/com/inspur/emmcloud/util/privates/VolumeFilePrivilegeUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/privates/VolumeFilePrivilegeUtils.java
@@ -50,35 +50,41 @@ public class VolumeFilePrivilegeUtils {
      * @return
      */
     public static boolean getVolumeFileWritable(Context context, VolumeFile volumeFile) {
-        int privilege = 0;
-        String myUid = MyApplication.getInstance().getUid();
-        LogUtils.jasonDebug("======myUid=" + myUid);
-        LogUtils.jasonDebug("=======volumeFile.getOwner()=" + volumeFile.getOwner());
-        if (volumeFile.getOwner().equals(myUid)) {
-            LogUtils.jasonDebug("===========isowner");
-            privilege = volumeFile.getOwnerPrivilege();
-        } else {
-            VolumeGroupContainMe volumeGroupContainMe = VolumeGroupContainMeCacheUtils.getVolumeGroupContainMe(context, volumeFile.getVolume());
-            if (volumeGroupContainMe != null) {
-                LogUtils.jasonDebug("===========有自己所属组数据");
-                List<String> groupIdList = volumeGroupContainMe.getGroupIdList();
-                List<Integer> privilegeList = new ArrayList<>();
-                for (int i = 0; i < groupIdList.size(); i++) {
-                    String groupId = groupIdList.get(i);
-                    Map<String, Integer> groupPrivilegeMap = volumeFile.getGroupPrivilegeMap();
-                    Integer groupPrivilege = groupPrivilegeMap.get(groupId);
-                    if (groupPrivilege != null) {
-                        privilegeList.add(groupPrivilege);
-                    }
-                }
-                privilege = Collections.max(privilegeList);
+        try {
+            int privilege = 0;
+            String myUid = MyApplication.getInstance().getUid();
+            LogUtils.jasonDebug("======myUid=" + myUid);
+            LogUtils.jasonDebug("=======volumeFile.getOwner()=" + volumeFile.getOwner());
+            if (volumeFile.getOwner().equals(myUid)) {
+                LogUtils.jasonDebug("===========isowner");
+                privilege = volumeFile.getOwnerPrivilege();
             } else {
-                LogUtils.jasonDebug("===========没有自己所属组数据");
-                privilege = volumeFile.getOthersPrivilege();
+                VolumeGroupContainMe volumeGroupContainMe = VolumeGroupContainMeCacheUtils.getVolumeGroupContainMe(context, volumeFile.getVolume());
+                if (volumeGroupContainMe != null) {
+                    LogUtils.jasonDebug("===========有自己所属组数据");
+                    List<String> groupIdList = volumeGroupContainMe.getGroupIdList();
+                    List<Integer> privilegeList = new ArrayList<>();
+                    for (int i = 0; i < groupIdList.size(); i++) {
+                        String groupId = groupIdList.get(i);
+                        Map<String, Integer> groupPrivilegeMap = volumeFile.getGroupPrivilegeMap();
+                        Integer groupPrivilege = groupPrivilegeMap.get(groupId);
+                        if (groupPrivilege != null) {
+                            privilegeList.add(groupPrivilege);
+                        }
+                    }
+                    privilege = Collections.max(privilegeList);
+                } else {
+                    LogUtils.jasonDebug("===========没有自己所属组数据");
+                    privilege = volumeFile.getOthersPrivilege();
+                }
             }
+            LogUtils.jasonDebug("===========最终privilege=" + privilege);
+            return (privilege > 4);
+        } catch (Exception e) {
+            LogUtils.jasonDebug("===========最终privilege Error::" + e.getMessage());
+            e.printStackTrace();
+            return false;
         }
-        LogUtils.jasonDebug("===========最终privilege=" + privilege);
-        return (privilege > 4);
     }
 
     /**
