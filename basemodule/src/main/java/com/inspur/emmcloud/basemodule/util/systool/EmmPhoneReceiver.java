49498f95986e047216f0ef1a2712a741863ebfb6
Yu Fuchang <yufuchang@inspur.com>
2019-11-29 14:26:46 +0800

    增加云+语音通话中，来了系统电话，则挂断云+语音通话功能 增加系统电话通话中，来了云+语音通话不唤起云+语音通话界面，发送拒绝消息的功能

---
+++ basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/systool/EmmPhoneReceiver.java
@@ -0,0 +1,42 @@
+package com.inspur.emmcloud.basemodule.util.systool;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+
+import com.inspur.emmcloud.baselib.router.Router;
+import com.inspur.emmcloud.componentservice.communication.CommunicationService;
+
+/**
+ * 小米6上测试，来电，去电，接听action都是：android.intent.action.PHONE_STATE
+ * Created by: yufuchang
+ * Date: 2019/11/29
+ */
+public class EmmPhoneReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //如果是去电
+        if (intent.getAction().equals(Intent.ACTION_NEW_OUTGOING_CALL)) {
+            Router router = Router.getInstance();
+            if (router.getService(CommunicationService.class) != null) {
+                CommunicationService service = router.getService(CommunicationService.class);
+                service.stopVoiceCommunication();
+            }
+        } else {
+            //注释来自网络资料
+            //查了下android文档，貌似没有专门用于接收来电的action,所以，非去电即来电.
+            //如果我们想要监听电话的拨打状况，需要这么几步 :
+            //第一：获取电话服务管理器TelephonyManager manager = this.getSystemService(TELEPHONY_SERVICE);
+            //第二：通过TelephonyManager注册我们要监听的电话状态改变事件。manager.listen(new EmmPhoneStateLinstener(),
+            //PhoneStateListener.LISTEN_CALL_STATE);这里的PhoneStateListener.LISTEN_CALL_STATE就是我们想要
+            //监听的状态改变事件，除此之外，还有很多其他事件。
+            //第三步：通过extends PhoneStateListener来定制自己的规则。将其对象传递给第二步作为参数。
+            //第四步：这一步很重要，那就是给应用添加权限。android.permission.READ_PHONE_STATE
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+            telephonyManager.listen(new EmmPhoneStateLinstener(), PhoneStateListener.LISTEN_CALL_STATE);
+        }
+    }
+}
