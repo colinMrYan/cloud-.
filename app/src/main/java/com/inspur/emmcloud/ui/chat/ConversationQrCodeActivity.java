cdf100a9640adafe8839f19e6009a85e4ce8354a
Yu Fuchang <yufuchang@inspur.com>
2019-12-07 14:55:28 +0800

    去掉扫码加群Logo

--- app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationQrCodeActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationQrCodeActivity.java
@@ -13,7 +13,6 @@ import com.inspur.emmcloud.R;
 import com.inspur.emmcloud.api.APIInterfaceInstance;
 import com.inspur.emmcloud.api.apiservice.ChatAPIService;
 import com.inspur.emmcloud.baselib.router.Router;
-import com.inspur.emmcloud.baselib.util.BitmapFillet;
 import com.inspur.emmcloud.baselib.util.ImageUtils;
 import com.inspur.emmcloud.baselib.util.IntentUtils;
 import com.inspur.emmcloud.baselib.util.JSONUtils;
@@ -345,10 +344,10 @@ public class ConversationQrCodeActivity extends BaseActivity {
             Router router = Router.getInstance();
             if (router.getService(com.inspur.emmcloud.componentservice.web.WebService.class) != null) {
                 com.inspur.emmcloud.componentservice.web.WebService service = router.getService(com.inspur.emmcloud.componentservice.web.WebService.class);
-                File file = new File(MyAppConfig.LOCAL_CACHE_PHOTO_PATH + "/" +
-                        MyApplication.getInstance().getTanent() + cid + "_100.png1");
-                Bitmap logoBitmap = ImageUtils.getBitmapByFile(file);
-                Bitmap bitmap = service.getQrCodeWithContent(scanCodeJoinConversationBean.getConversationQrCode(), BitmapFillet.fillet(logoBitmap, 15, BitmapFillet.CORNER_ALL), SHARE_QR_CODE_SIZE);
+//                File file = new File(MyAppConfig.LOCAL_CACHE_PHOTO_PATH + "/" +
+//                        MyApplication.getInstance().getTanent() + cid + "_100.png1");
+//                Bitmap logoBitmap = ImageUtils.getBitmapByFile(file);
+                Bitmap bitmap = service.getQrCodeWithContent(scanCodeJoinConversationBean.getConversationQrCode(), SHARE_QR_CODE_SIZE);
                 //测试代码，写死地址测试跳转到群消息页面，并拉消息，排序会话列表
 //                Bitmap bitmap = service.getQrCodeWithContent("http://emm.inspuronline.com:83/demo/join_group.html", BitmapFillet.fillet(logoBitmap, 15, BitmapFillet.CORNER_ALL), SHARE_QR_CODE_SIZE);
                 groupQrCodeImage.setImageBitmap(bitmap);
