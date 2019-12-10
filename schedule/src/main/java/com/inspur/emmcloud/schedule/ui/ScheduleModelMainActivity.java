0c9625d9b5b078af837cb3b4250896035a43a857
libaochao <libaochao@inspur.com>
2019-12-10 09:02:23 +0800

    Work 模块化整理

---
+++ schedule/src/main/java/com/inspur/emmcloud/schedule/ui/ScheduleModelMainActivity.java
@@ -0,0 +1,19 @@
+package com.inspur.emmcloud.schedule.ui;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.inspur.emmcloud.schedule.R;
+
+/**
+ * Created by libaochao on 2019/12/9.
+ */
+
+public class ScheduleModelMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.schedule_model_main_activity);
+    }
+}
---
+++ schedule/src/main/runalone/java/com/inspur/emmcloud/schedule/runalone/application/ScheduleApplication.java
@@ -0,0 +1,22 @@
+package com.inspur.emmcloud.schedule.runalone.application;
+
+
+import com.inspur.emmcloud.basemodule.application.BaseApplication;
+import com.inspur.emmcloud.basemodule.config.Constant;
+
+/**
+ * Created by chenmch on 2019/5/7.
+ */
+
+public class ScheduleApplication extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public String getIntentClassRouterAfterLogin() {
+        return Constant.AROUTER_CLASS_MAIL_LOGIN;
+    }
+}
