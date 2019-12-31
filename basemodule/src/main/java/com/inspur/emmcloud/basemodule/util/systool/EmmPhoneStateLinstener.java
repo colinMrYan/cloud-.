4f55096c348940ffc7cfeed24c0e2890be15449d
Yu Fuchang <yufuchang@inspur.com>
2019-12-31 09:20:18 +0800

    云+在语音通话中，进来系统语音通话，当系统语音通话接听时，挂断云+语音通话，增加广播注释

--- basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/systool/EmmPhoneStateLinstener.java
+++ basemodule/src/main/java/com/inspur/emmcloud/basemodule/util/systool/EmmPhoneStateLinstener.java
@@ -23,14 +23,10 @@ public class EmmPhoneStateLinstener extends PhoneStateListener {
         //注意，方法必须写在super方法后面，否则incomingNumber无法获取到值，想获取到值仍然需要动态获取电话（接听或挂断电话、监听通话状态）。
         //此处只需要得到电话的忙闲状态，所以暂时不考虑获取以上权限，微信也没有获取此权限，推断微信应该也只得到了电话的忙闲状态
         super.onCallStateChanged(state, incomingNumber);
-        //如果电话不是空闲状态，就挂断云+语音通话
-        //如果是空闲状态则此处不做处理，走后续云+语音通话逻辑
         switch (state) {
             case TelephonyManager.CALL_STATE_IDLE:
-//             System.out.println("挂断");
                 break;
             case TelephonyManager.CALL_STATE_OFFHOOK:
-//            System.out.println("接听");
                 Router router = Router.getInstance();
                 if (router.getService(CommunicationService.class) != null) {
                     CommunicationService service = router.getService(CommunicationService.class);
@@ -38,11 +34,13 @@ public class EmmPhoneStateLinstener extends PhoneStateListener {
                 }
                 break;
             case TelephonyManager.CALL_STATE_RINGING:
-//             System.out.println("响铃:来电号码"+incomingNumber);
-                //输出来电号码
+                break;
+            default:
                 break;
         }
 
+        //如果电话不是空闲状态，就挂断云+语音通话
+        //如果是空闲状态则此处不做处理，走后续云+语音通话逻辑
 //        if (state != TelephonyManager.CALL_STATE_IDLE) {
 //            Router router = Router.getInstance();
 //            if (router.getService(CommunicationService.class) != null) {
