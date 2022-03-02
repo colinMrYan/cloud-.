d60b8bde4952bb1bb213aa9711704f0ab71c5508
陈浩 <chenhao06@inspur.com>
2022-03-02 10:29:20 +0800

    feat:网络变化时增加提示

--- app/src/main/java/com/inspur/emmcloud/util/privates/NetWorkStateChangeUtils.java
+++ app/src/main/java/com/inspur/emmcloud/util/privates/NetWorkStateChangeUtils.java
@@ -5,9 +5,12 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
 import com.inspur.emmcloud.MyApplication;
+import com.inspur.emmcloud.baselib.util.ResourceUtils;
+import com.inspur.emmcloud.baselib.util.ToastUtils;
 import com.inspur.emmcloud.basemodule.util.AppBadgeUtils;
 import com.inspur.emmcloud.basemodule.util.CheckingNetStateUtils;
 import com.inspur.emmcloud.basemodule.util.NetUtils;
+import com.inspur.emmcloud.basemodule.util.Res;
 import com.inspur.emmcloud.push.WebSocketPush;
 
 /**
@@ -18,8 +21,8 @@ public class NetWorkStateChangeUtils {
 
 
     private static NetWorkStateChangeUtils netWorkStateChangeUtils;
-    private CheckingNetStateUtils checkingNetStateUtils = new CheckingNetStateUtils(MyApplication.getInstance(), NetUtils.pingUrls,(new NetUtils()).getHttpUrls());
-
+    private CheckingNetStateUtils checkingNetStateUtils = new CheckingNetStateUtils(MyApplication.getInstance(), NetUtils.pingUrls, (new NetUtils()).getHttpUrls());
+    private static boolean isColdReboot = true;
 
     private NetWorkStateChangeUtils() {
 
@@ -39,32 +42,34 @@ public class NetWorkStateChangeUtils {
     public void netWorkStateChange() {
         try {
             boolean isAppOnForeground = MyApplication.getInstance().getIsActive();
-            if (isAppOnForeground){
+            if (isAppOnForeground) {
                 boolean isConnected = false;
                 ConnectivityManager connectivity = (ConnectivityManager) MyApplication.getInstance()
                         .getSystemService(Context.CONNECTIVITY_SERVICE);
                 if (connectivity != null) {
                     NetworkInfo[] info = connectivity.getAllNetworkInfo();
                     if (info != null) {
-                        for (int i = 0; i < info.length; i++) {
-                            if (info[i].getState() == NetworkInfo.State.CONNECTED || info[i].getState() == NetworkInfo.State.CONNECTING) {
+                        for (NetworkInfo networkInfo : info) {
+                            if (networkInfo.getState() == NetworkInfo.State.CONNECTED || networkInfo.getState() == NetworkInfo.State.CONNECTING) {
                                 isConnected = true;
                                 break;
                             }
                         }
                     }
                 }
-
-                if (isConnected){
+                if (isConnected) {
                     getBadgeFromServer(MyApplication.getInstance());
                     WebSocketPush.getInstance().startWebSocket();
                 }
                 checkingNetStateUtils = new CheckingNetStateUtils(MyApplication.getInstance(), NetUtils.pingUrls, (new NetUtils()).getHttpUrls());
                 checkingNetStateUtils.getNetStateResult(5);
+                if (!isColdReboot) {
+                    ToastUtils.show(Res.getString("network_change_tip") + checkingNetStateUtils.getNetworksType());
+                }
+                isColdReboot = false;
             }
-
         } catch (Exception e) {
-           e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
