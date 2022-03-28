9901e50f0842ffdda6ffd83f64ad1a7527caee1f
赵德栋 <zhaodedong@inspur.com>
2022-03-28 18:05:11 +0800

    feat：更新阅后即焚和悄悄话消息类型UI

--- app/src/main/java/com/inspur/emmcloud/ui/chat/DisplayTextBurnMsg.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/DisplayTextBurnMsg.java
@@ -21,7 +21,7 @@ public class DisplayTextBurnMsg {
 
     public static View getView(final Context context, Message message) {
         View cardContentView = LayoutInflater.from(context).inflate(
-                R.layout.chat_msg_card_child_text_plain_view, null);
+                R.layout.chat_msg_card_child_text_burn_view, null);
         final boolean isMyMsg = message.getFromUser().equals(
                 MyApplication.getInstance().getUid());
         BubbleLayout cardLayout = cardContentView.findViewById(R.id.bl_card);
--- app/src/main/java/com/inspur/emmcloud/ui/chat/DisplayTextWhisperMsg.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/DisplayTextWhisperMsg.java
@@ -21,7 +21,7 @@ public class DisplayTextWhisperMsg {
 
     public static View getView(final Context context, Message message) {
         View cardContentView = LayoutInflater.from(context).inflate(
-                R.layout.chat_msg_card_child_text_plain_view, null);
+                R.layout.chat_msg_card_child_text_whisper_view, null);
         final boolean isMyMsg = message.getFromUser().equals(
                 MyApplication.getInstance().getUid());
         BubbleLayout cardLayout = cardContentView.findViewById(R.id.bl_card);
