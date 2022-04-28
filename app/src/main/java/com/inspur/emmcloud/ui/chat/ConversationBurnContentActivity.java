ab9e542f58e14fc23ddf9f57f454e663e9aa2154
赵德栋 <zhaodedong@inspur.com>
2022-04-28 19:35:01 +0800

    fix：462284 阅后即焚表情显示为文字，非表情图。例如：[晕][龇牙]

--- app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationBurnContentActivity.java
+++ app/src/main/java/com/inspur/emmcloud/ui/chat/ConversationBurnContentActivity.java
@@ -1,16 +1,21 @@
 package com.inspur.emmcloud.ui.chat;
 
+import android.text.SpannableString;
 import android.view.View;
 import android.widget.TextView;
+
+import com.amazonaws.mobile.auth.core.signin.ui.DisplayUtils;
 import com.inspur.emmcloud.R;
 import com.inspur.emmcloud.basemodule.ui.BaseActivity;
+import com.inspur.emmcloud.ui.chat.emotion.EmotionUtil;
 
 public class ConversationBurnContentActivity extends BaseActivity {
 
     @Override
     public void onCreate() {
         TextView view = findViewById(R.id.content);
-        view.setText(getIntent().getStringExtra("content"));
+        SpannableString spannableString = new SpannableString(getIntent().getStringExtra("content"));
+        view.setText(EmotionUtil.getInstance(this).getSmiledText(spannableString, DisplayUtils.dp(20)));
     }
 
     @Override
